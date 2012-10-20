5985666e51f429d19807a2c6bc92c69d001694cb
Brian McFee <brm2132@columbia.edu>
2012-10-20 19:37:14 -0400

    istft is implemented, but not correct yet

--- librosa/stft.py
+++ librosa/stft.py
@@ -28,34 +28,71 @@ def __make_hann_window(w, f):
 
 
 
-def stft(x, n_fft=256, hann_window=None, hop_size=None, sample_rate=8000):
+def stft(x, n_fft=256, hann_window=None, hop=None, sample_rate=8000):
+
+    num_samples = len(x)
 
     if hann_window is None:
         hann_window = n_fft
         pass
 
-    
-    num_samples = len(x)
-
     if hann_window == 0:
         window = numpy.ones((n_fft,))
-        pass
-    elif hann_window > 0:
+    else:
         window = __make_hann_window(hann_window, n_fft)
         pass
 
-    # Set the default hop_size, if it's not already specified
-    if hop_size is None:
-        hop_size = int(window.shape[0] / 2)
+    # Set the default hop, if it's not already specified
+    if hop is None:
+        hop = int(window.shape[0] / 2)
         pass
 
     # allocate output array
-    D = numpy.zeros( (int(1 + n_fft / 2), 1 + int( ( num_samples - n_fft) / hop_size) ), dtype=numpy.complex)
+    D = numpy.zeros( (int(1 + n_fft / 2), 1 + int( ( num_samples - n_fft) / hop) ), dtype=numpy.complex)
 
-    for (i, b) in enumerate(xrange(0, num_samples - n_fft, hop_size)):
+    for (i, b) in enumerate(xrange(0, num_samples - n_fft, hop)):
         u           = window * x[b:(b+n_fft)]
         t           = scipy.fft(u)
         D[:,i]      = t[:1+n_fft/2]
         pass
 
     return D
+
+
+def istft(d, n_fft=None, hann_window=None, hop=None):
+    
+    num_frames = d.shape[1]
+
+    if n_fft is None:
+        n_fft = 2 * (d.shape[0] - 1)
+        pass
+
+    if hann_window is None:
+        hann_window = n_fft
+        pass
+
+
+    if hann_window == 0:
+        window = numpy.ones((n_fft,))
+    else:
+        # FIXME:   2012-10-20 18:58:56 by Brian McFee <brm2132@columbia.edu>
+        #      there's a magic number 2/3 in istft.m ... not sure about this one
+        window = __make_hann_window(hann_window, n_fft) * 2.0 / 3
+        pass
+
+    # Set the default hop, if it's not already specified
+    if hop is None:
+        hop = int(window.shape[0] / 2 )
+        pass
+
+    x_length    = n_fft + (num_frames - 1) * hop
+    x           = numpy.zeros((x_length,))
+
+    for b in xrange(0, hop * (num_frames - 1), hop):
+        ft              = d[:, b/hop]
+        ft              = numpy.concatenate((ft, numpy.conj(ft[n_fft/2:1:-1])), 0)
+        px              = numpy.real(scipy.ifft(ft))
+        x[b:(b+n_fft)] += px * window
+        pass
+
+    return x
