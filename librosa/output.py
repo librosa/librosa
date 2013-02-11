f709396d199afa4bb2dee6c0b2fb95e3927c264e
Brian McFee <brm2132@columbia.edu>
2013-02-11 16:03:03 -0500

    beat tracker works

--- librosa/output.py
+++ librosa/output.py
@@ -29,9 +29,31 @@ def segment_csv(outfile, N, sr, hop_length):
         CW = csv.writer(f)
 
         for segment in N:
-            t_segment = librosa.beat.frames_to_time(segment, sr=sr, hop_length=hop_length)
+            t_segment = librosa.frames_to_time(segment, sr=sr, hop_length=hop_length)
             CW.writerow([t, '%d frames' % segment])
             t += t_segment
+            pass
+        pass
+    pass
+
+def beat_csv(outfile, beats, sr, hop_length):
+    '''
+    Save beat tracker output in CSV format
+
+    Input:
+        outfile:            path to the output file
+        beats:              1-by-n list of frame numbers for beat events
+        sr:                 sample rate of the beat detector (eg 8000)
+        hop_length:         hop length of the beat detector (32)
+    '''
+
+    with open(outfile, 'w') as f:
+        CW = csv.writer(f)
+
+        t = 0.0
+        for t_new in librosa.frames_to_time(beats, sr=sr, hop_length=hop_length):
+            CW.writerow([t_new, '%.2f BPM' % (60.0 / (t_new - t))])
+            t = t_new
             pass
         pass
     pass
