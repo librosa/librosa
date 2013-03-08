c6891f63f523b0af5109b79111f4d442fe78672f
Brian McFee <brm2132@columbia.edu>
2013-03-08 16:32:31 -0500

    streamlined test code

--- tests/testLibrosaCore.py
+++ tests/testLibrosaCore.py
@@ -12,24 +12,24 @@ import numpy, scipy.io
 
 from nose.tools import nottest
 
-def test_hz_to_mel():
-    test_files = glob.glob('data/hz_to_mel*.mat')
+def fileGenerator(pattern):
+    test_files = glob.glob(pattern)
     test_files.sort()
+    return test_files
 
+def test_hz_to_mel():
     def __check_hz_to_mel(infile):
         DATA    = scipy.io.loadmat(infile)
         z       = librosa.hz_to_mel(DATA['f'], DATA['htk'])
 
         assert numpy.allclose(z, DATA['result'])
     
-    for infile in test_files:
+    for infile in fileGenerator('data/*hz_to_mel-*.mat'):
         yield (__check_hz_to_mel, infile)
-        pass
+
     pass
 
 def test_mel_to_hz():
-    test_files = glob.glob('data/mel_to_hz*.mat')
-    test_files.sort()
 
     def __check_mel_to_hz(infile):
         DATA    = scipy.io.loadmat(infile)
@@ -37,30 +37,24 @@ def test_mel_to_hz():
 
         assert numpy.allclose(z, DATA['result'])
     
-    for infile in test_files:
+    for infile in fileGenerator('data/*mel_to_hz-*.mat'):
         yield (__check_mel_to_hz, infile)
-        pass
+
     pass
 
 def test_hz_to_octs():
-    test_files = glob.glob('data/hz_to_octs*.mat')
-    test_files.sort()
-
     def __check_hz_to_octs(infile):
         DATA    = scipy.io.loadmat(infile)
         z       = librosa.hz_to_octs(DATA['f'])
 
         assert numpy.allclose(z, DATA['result'])
 
-    for infile in test_files:
+    for infile in fileGenerator('data/*hz_to_octs-*.mat'):
         yield (__check_hz_to_octs, infile)
-        pass
+
     pass
 
 def test_load():
-    test_files = glob.glob('data/load*.mat')
-    test_files.sort()
-
     # Note: this does not test resampling.
     # That is a separate unit test.
 
@@ -74,8 +68,8 @@ def test_load():
         # Transpose here because matlab is row-oriented
         assert numpy.allclose(y, DATA['y'].T)
 
-    for infile in test_files:
+    for infile in fileGenerator('data/*load-*.mat'):
         yield (__check_load, infile)
-        pass
+
     pass
 
