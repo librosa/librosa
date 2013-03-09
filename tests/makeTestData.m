a4d01f53cf17bbb2109161d926decac0d74bbe2f
Brian McFee <brm2132@columbia.edu>
2013-03-08 19:21:30 -0500

    making different test files

--- tests/makeTestData.m
+++ tests/makeTestData.m
@@ -1,5 +1,5 @@
-function makeTestData(source_path, output_path)
-% makeTestData(source_path, audio_file, output_path)
+function testData(source_path, output_path)
+% testData(source_path, audio_file, output_path)
 %   source_path = path to DPWE code
 %   output_ptah = directory to store generated files
 %
@@ -20,28 +20,35 @@ function makeTestData(source_path, output_path)
 %       istft
 %
 %       load
+%       resample
 %
 
     % Make sure we have the path to DPWE code
     addpath(source_path);
 
     display('hz_to_mel');
-    makeTestHz2Mel(output_path);
+    testHz2Mel(output_path);
 
     display('mel_to_hz');
-    makeTestMel2Hz(output_path);
+    testMel2Hz(output_path);
 
     display('hz_to_octs');
-    makeTestHzToOcts(output_path);
+    testHzToOcts(output_path);
 
     display('load');
-    makeTestLoad(output_path);
+    testLoad(output_path);
+
+%     display('melfb');
+%     testMelfb(output_path);
+
+    display('resample');
+    testResample(output_path);
 
     %% Done!
     display('Done.');
 end
 
-function makeTestHz2Mel(output_path)
+function testHz2Mel(output_path)
 
     % Test with either a scalar argument or a vector
     P_HZ = {[440], [2.^(1:13)]};
@@ -70,7 +77,7 @@ function makeTestHz2Mel(output_path)
     end
 end
 
-function makeTestMel2Hz(output_path)
+function testMel2Hz(output_path)
 
     % Test with either a scalar argument or a vector
     P_MELS      = {[5], [2.^(-2:9)]};
@@ -99,7 +106,7 @@ function makeTestMel2Hz(output_path)
     end
 end
 
-function makeTestHzToOcts(output_path)
+function testHzToOcts(output_path)
 
     % Scalar argument or a vector
     P_HZ      = {[5], [2.^(2:14)]};
@@ -121,13 +128,13 @@ function makeTestHzToOcts(output_path)
     end
 end
 
-function makeTestLoad(output_path)
+function testLoad(output_path)
 
     % Test: load a wav file
     %       get audio stream (floats) and sample rate
     %       preserve stereo or convert to mono
-    infile          = 'data/test1.wav';
-    [y, sr]         = wavread(infile);
+    wavfile          = 'data/test1_44100.wav';
+    [y, sr]         = wavread(wavfile);
     mono            = 0;
 
     % Stereo output
@@ -135,7 +142,7 @@ function makeTestLoad(output_path)
 
     filename = sprintf('%s/load-%03d.mat', output_path, counter);
     display(['  `-- saving ', filename]);
-    save(filename, 'infile', 'mono', 'y', 'sr');
+    save(filename, 'wavfile', 'mono', 'y', 'sr');
 
     % Mono output
     counter = 2;
@@ -143,6 +150,102 @@ function makeTestLoad(output_path)
     y       = mean(y, 2);
     filename = sprintf('%s/load-%03d.mat', output_path, counter);
     display(['  `-- saving ', filename]);
-    save(filename, 'infile', 'mono', 'y', 'sr');
+    save(filename, 'wavfile', 'mono', 'y', 'sr');
+
+end
+
+function testMelfb(output_path)
+
+    % Test three sample rates
+    P_SR        = [8000, 11025, 22050];
+
+    % Two FFT lengths
+    P_NFFT      = [256, 516];
+
+    % Three filter bank sizes
+    P_NFILTS    = [20, 40, 120];
+
+    % Two widths
+    P_WIDTH     = [1.0, 3.0];
+
+    % F_min
+    P_FMIN      = [0, 512];
+
+    % F_max
+    P_FMAX      = [2000, inf];
+
+    % Slaney or HTK mels
+    P_HTK       = [0, 1];
+
+    % Generate tests
+    counter = 0;
+
+    for sr = P_SR
+        for nfft = P_NFFT
+            for nfilts = P_NFILTS
+                for width = P_WIDTH
+                    for fmin = P_FMIN
+                        for fmax = P_FMAX
+                            if isinf(fmax)
+                                fmax = sr / 2;
+                            end
+                            for htk = P_HTK
+
+            % Run the function
+            [wts, frqs] = fft2melmx(sr, nfft, nfilts, width, fmin, fmax, htk, 0);
+
+            % save the output
+            counter = counter + 1;
+
+            filename = sprintf('%s/melfb-%03d.mat', output_path, counter);
+            display(['  `-- saving ', filename]);
+
+            save(filename, ...
+                    'sr', 'nfft', 'nfilts', 'width', ...
+                    'fmin', 'fmax', 'htk', 'wts', 'frqs');
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+end
+
+function testResample(output_path)
+
+    wavfile         = 'data/test1_22050.wav';
+
+    [y_in, sr_in]   = wavread(wavfile);
+    y_in            = mean(y_in, 2);        % Convert to mono
+
+    P_SR            = [8000, 22050, 44100];
+
+    counter = 0;
+
+    for sr_out = P_SR
+
+        y_out       = resample(y_in, sr_out, sr_in);
+
+        counter     = counter + 1;
+        filename    = sprintf('%s/resample-%03d.mat', output_path, counter);
+
+        display(['  `-- saving ', filename]);
+
+        save(filename, 'wavfile', 'y_in', 'sr_in', 'y_out', 'sr_out');
+    end
+end
+
+function testSTFT(output_path)
+
+    wavfile     = 'data/test1_22050.wav';
+
+    [y, sr]     = wavread(wavfile);
+    y           = mean(y_in, 2);        % Convert to mono
+
+
+
+
 
 end
--- tests/testLibrosaCore.py
+++ tests/testLibrosaCore.py
@@ -12,45 +12,45 @@ import numpy, scipy.io
 
 from nose.tools import nottest
 
-def fileGenerator(pattern):
+def files(pattern):
     test_files = glob.glob(pattern)
     test_files.sort()
     return test_files
 
 def test_hz_to_mel():
-    def __check_hz_to_mel(infile):
+    def __test_to_mel(infile):
         DATA    = scipy.io.loadmat(infile)
         z       = librosa.hz_to_mel(DATA['f'], DATA['htk'])
 
         assert numpy.allclose(z, DATA['result'])
     
-    for infile in fileGenerator('data/*hz_to_mel-*.mat'):
-        yield (__check_hz_to_mel, infile)
+    for infile in files('data/*hz_to_mel-*.mat'):
+        yield (__test_to_mel, infile)
 
     pass
 
 def test_mel_to_hz():
 
-    def __check_mel_to_hz(infile):
+    def __test_to_hz(infile):
         DATA    = scipy.io.loadmat(infile)
         z       = librosa.mel_to_hz(DATA['f'], DATA['htk'])
 
         assert numpy.allclose(z, DATA['result'])
     
-    for infile in fileGenerator('data/*mel_to_hz-*.mat'):
-        yield (__check_mel_to_hz, infile)
+    for infile in files('data/*mel_to_hz-*.mat'):
+        yield (__test_to_hz, infile)
 
     pass
 
 def test_hz_to_octs():
-    def __check_hz_to_octs(infile):
+    def __test_to_octs(infile):
         DATA    = scipy.io.loadmat(infile)
         z       = librosa.hz_to_octs(DATA['f'])
 
         assert numpy.allclose(z, DATA['result'])
 
-    for infile in fileGenerator('data/*hz_to_octs-*.mat'):
-        yield (__check_hz_to_octs, infile)
+    for infile in files('data/*hz_to_octs-*.mat'):
+        yield (__test_to_octs, infile)
 
     pass
 
@@ -58,9 +58,9 @@ def test_load():
     # Note: this does not test resampling.
     # That is a separate unit test.
 
-    def __check_load(infile):
+    def __test(infile):
         DATA    = scipy.io.loadmat(infile)
-        (y, sr) = librosa.load(DATA['infile'][0], target_sr=None, mono=DATA['mono'])
+        (y, sr) = librosa.load(DATA['wavfile'][0], target_sr=None, mono=DATA['mono'])
 
         # Verify that the sample rate is correct
         assert sr == DATA['sr']
@@ -68,8 +68,56 @@ def test_load():
         # Transpose here because matlab is row-oriented
         assert numpy.allclose(y, DATA['y'].T)
 
-    for infile in fileGenerator('data/*load-*.mat'):
-        yield (__check_load, infile)
-
+    for infile in files('data/*load-*.mat'):
+        yield (__test, infile)
     pass
 
+def test_resample():
+
+    def __test(infile):
+        DATA    = scipy.io.loadmat(infile)
+        
+        # load the wav file
+        (y_in, sr_in) = librosa.load(DATA['wavfile'][0], target_sr=None, mono=True)
+
+        # Resample it to the target rate
+        y_out = librosa.resample(y_in, DATA['sr_in'], DATA['sr_out'])
+
+        # Are we the same length?
+        if len(y_out) == len(DATA['y_out']):
+            # Is the data close?
+            assert numpy.allclose(y_out, DATA['y_out'].T)
+        elif len(y_out) == len(DATA['y_out']) - 1:
+            assert (numpy.allclose(y_out, DATA['y_out'][:-1,0]) or
+                    numpy.allclose(y_out, DATA['y_out'][1:,0]))
+        elif len(y_out) == len(DATA['y_out']) + 1:
+            assert (numpy.allclose(y_out[1:], DATA['y_out'].T) or
+                    numpy.allclose(y_out[:-2], DATA['y_out'].T))
+        else:
+            assert False
+        pass
+
+
+    for infile in files('data/*resample-*.mat'):
+        yield (__test, infile)
+    pass
+
+@nottest
+def test_melfb():
+
+    def __test(infile):
+        DATA    = scipy.io.loadmat(infile)
+
+        wts = librosa.melfb(    DATA['sr'], 
+                                DATA['nfft'], 
+                                nfilts  =   DATA['nfilts'],
+                                width   =   DATA['width'],
+                                fmin    =   DATA['fmin'],
+                                fmax    =   DATA['fmax'],
+                                use_htk =   DATA['htk'])
+                                
+        assert numpy.allclose(wts, DATA['wts'])
+
+    for infile in files('data/*melfb-*.mat'):
+        yield (__test, infile)
+    pass
