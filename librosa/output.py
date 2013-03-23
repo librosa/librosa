c1a1908e87e187b8e27e44ea3576bd42787cf882
Brian McFee <brm2132@columbia.edu>
2013-03-23 09:14:27 -0400

    linted output

--- librosa/output.py
+++ librosa/output.py
@@ -14,40 +14,38 @@ import numpy, scipy
 import scipy.io.wavfile
 import csv
 
-def segment_csv(outfile, segments, sr, hop_length):
+def segment_csv(path, segments, sr, hop_length):
     '''
     Save beat tracker or segmentation output in CSV format
 
     Input:
-        outfile:            path to the output file
+        path:               path to the output file
         segments:           1-by-n list of frame numbers for beat events
         sr:                 sample rate of the beat detector (eg 8000)
         hop_length:         hop length of the beat detector (32)
     '''
 
-    with open(outfile, 'w') as f:
-        CW = csv.writer(f)
+    with open(path, 'w') as output_file:
+        writer = csv.writer(output_file)
 
-        t = 0.0
-        for t_new in librosa.frames_to_time(segments, sr=sr, hop_length=hop_length):
-            CW.writerow([t_new, '%.2f BPM' % (60.0 / (t_new - t))])
-            t = t_new
-            pass
-        pass
-    pass
+        time = 0.0
+        for t_new in librosa.frames_to_time(segments,
+                                            sr=sr, hop_length=hop_length):
+            writer.writerow([t_new, '%.2f BPM' % (60.0 / (t_new - time))])
+            time = t_new
+#-- --#
 
-
-def write_wav(y, sr, filename):
+def write_wav(path, y, sr):
     '''
     Output a time series as a .wav file
 
     Input:
+        path:       path to save the wav file. 
         y:          time-series audio data (floating point) (mono)
         sr:         sample rate of y
-        filename:   path to save the wav file. 
 
     '''
 
-    w = y / numpy.max(numpy.abs(y))
-    scipy.io.wavfile.write(filename, sr, (w * 32768.0).astype('<i2'))
-    pass
+    wav = y / numpy.max(numpy.abs(y))
+    scipy.io.wavfile.write(path, sr, (wav * 32768.0).astype('<i2'))
+#-- --#
