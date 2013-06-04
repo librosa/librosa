bfed47df61d49bf78856324982419896982605b5
Brian McFee <brm2132@columbia.edu>
2013-06-04 16:03:07 -0400

    updates to the analyzer

--- examples/analyzer.py
+++ examples/analyzer.py
@@ -13,9 +13,10 @@ import sys
 import ujson as json
 
 import numpy as np
+import scipy, scipy.signal
 import librosa
 
-HOP = 64
+HOP = 128
 SR  = 22050
 
 def analyze_file(infile):
@@ -45,6 +46,16 @@ def analyze_file(infile):
     A['tempo'] = tempo
     A['beats'] = librosa.frames_to_time(beats, sr, hop_length=HOP).tolist()
 
+    
+    S = librosa.feature.melspectrogram(y, sr,   n_fft=2048, 
+                                                hop_length=HOP, 
+                                                n_mels=64, 
+                                                fmin=37, 
+                                                fmax=8000)
+    S = S / S.max()
+
+    A['spectrogram'] = librosa.feature.sync(librosa.logamplitude(S**2), beats).T.tolist()
+
     # Let's make some beat-synchronous mfccs
     S = librosa.feature.melspectrogram(y, sr, hop_length=HOP)
     S = librosa.feature.mfcc(librosa.logamplitude(S))
@@ -52,9 +63,30 @@ def analyze_file(infile):
 
     # And some chroma
     S = np.abs(librosa.stft(y, hop_length=HOP))
-    A['pitches'] = librosa.feature.sync(librosa.feature.chromagram(S, sr),
+
+    # Grab the harmonic component
+    H = librosa.hpss.hpss_median(S, win_P=31, win_H=31, p=1.0)[0]
+    A['chroma'] = librosa.feature.sync(librosa.feature.chromagram(H, sr),
                                         beats,
                                         aggregate=np.median).T.tolist()
+
+    # Harmonicity: ratio of H::S averaged per frame
+    A['harmonicity'] = librosa.feature.sync(np.mean(H / (S + (S==0)), axis=0, keepdims=True),
+                                            beats,
+                                            aggregate=np.max).flatten().tolist()
+
+
+    # Relative loudness
+    S = S / S.max()
+    S = S**2
+
+    A['loudness'] = librosa.feature.sync(np.max(librosa.logamplitude(S), 
+                                                axis=0,
+                                                keepdims=True), 
+                                         beats, aggregate=np.max).flatten().tolist()
+
+    # Subsample the signal for vis purposes
+    A['signal'] = scipy.signal.decimate(y, len(y) / 1024, ftype='fir').tolist()
 
     return A
 
