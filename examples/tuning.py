6fe3a31dbcd0233372017e813e0e27fe20099978
Brian McFee <brm2132@columbia.edu>
2013-12-10 15:33:25 -0500

    updated ifptrack docs, tuning example

--- examples/tuning.py
+++ examples/tuning.py
@@ -4,34 +4,45 @@ CREATED:2013-12-09 00:02:54 by Brian McFee <brm2132@columbia.edu>
  
 Estimate the tuning (deviation from A440) of a recording.
 
-Usage:
-
-./tuning.py input_file
+Usage: ./tuning.py [-h] input_file
 '''
 
-import sys, librosa
+import sys, argparse
 import numpy as np
 
-N_FFT       = 2048
-HOP_LENGTH  = N_FFT / 4
+import librosa
 
-print 'Loading ', sys.argv[1], '... ',
-y, sr = librosa.load(sys.argv[1])
-print 'done.'
+def estimate_tuning(input_file):
+    print 'Loading ', input_file
+    y, sr = librosa.load(input_file)
 
-# Get the ifptrack
-print 'Tracking pitches... ',
-pitches, magnitudes, D = librosa.feature.ifptrack(  y, sr, 
-                                                    n_fft=N_FFT, 
-                                                    hop_length=HOP_LENGTH)
-print 'done.'
+    # Get the instantaneous-frequency pitch track
+    print 'Tracking pitches... ',
+    pitches, magnitudes, D = librosa.feature.ifptrack(  y, sr )
 
-# Just track the pitches associated with high magnitude
-print 'Estimating tuning ... ',
-pitches = pitches[magnitudes > np.median(magnitudes)]
+    print 'Estimating tuning ... ',
+    # Just track the pitches associated with high magnitude
+    pitches = pitches[magnitudes > np.median(magnitudes)]
 
-tuning = librosa.feature.estimate_tuning(pitches)
-print 'done.'
+    tuning = librosa.feature.estimate_tuning(pitches)
+    print '%+0.2f cents' % (100 * tuning)
 
-print sys.argv[1], ': %+0.2f cents' % (100 * tuning)
+
+def process_arguments():
+    '''Argparse function to get the program parameters'''
+
+    parser = argparse.ArgumentParser(description='librosa tuning estimation example')
+
+    parser.add_argument(    'input_file',
+                            action      =   'store',
+                            help        =   'path to the input file (wav, mp3, etc)')
+
+    return vars(parser.parse_args(sys.argv[1:]))
    
+
+if __name__ == '__main__':
+    # Get the parameters
+    parameters = process_arguments()
+
+    # Run the beat tracker
+    estimate_tuning(parameters['input_file'])
