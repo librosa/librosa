dd1437677d376386d123271ef82df025ca30e6a0
Brian McFee <brm2132@columbia.edu>
2013-12-12 17:45:47 -0500

    examples in onset detection

--- librosa/onset.py
+++ librosa/onset.py
@@ -11,12 +11,21 @@ import librosa.feature
 def onset_detect(y=None, sr=22050, onset_envelope=None, hop_length=64, **kwargs):
     """Basic onset detector
         
+    Example usage:
+        >>> # Get onset times from a signal
+        >>> onset_frames    = librosa.onset.onset_detect(y=y, sr=sr, hop_length=64)
+        >>> onset_times     = librosa.frames_to_time(onset_frames, sr, hop_length=64)
+
+        >>> # Or use a pre-computed onset envelope
+        >>> onsets          = librosa.onset.onset_strength(y, sr)
+        >>> onset_frames    = librosa.onset.onset_detect(onset_envelope=onsets, sr=sr)
+
     :parameters:
       - y          : np.ndarray
           audio time series
     
       - sr         : int
-          sampling rate of y
+          sampling rate of ``y``
     
       - onset_envelope     : np.ndarray
           (optional) pre-computed onset stength envelope
@@ -83,12 +92,25 @@ def onset_strength(y=None, sr=22050, S=None, detrend=False, feature=librosa.feat
 
     By default, if a time series is provided, S will be the log-power Mel spectrogram.
 
-    :parametrs:
+    Example usage:
+        >>> # Mean aggregation with Mel-scaled spectrogram
+        >>> onsets = librosa.onset.onset_strength(y, sr)
+
+        >>> # Median aggregation
+        >>> onsets = librosa.onset.onset_strength(y, sr, aggregate=np.median)
+
+        >>> # Log-frequency spectrogram instead of Mel
+        >>> onsets = librosa.onset.onset_strength(y, sr, feature=librosa.feature.logfsgram)
+
+        >>> # Or Mel spectrogram with customized options
+        >>> onsets = librosa.onset.onset_strength(y, sr, n_mels=128, fmin=32, fmax=8000)
+
+    :parameters:
       - y        : np.ndarray
           audio time-series
 
       - sr       : int
-          sampling rate of y
+          sampling rate of ``y``
 
       - S        : np.ndarray 
           pre-computed (log-power) spectrogram
@@ -105,17 +127,17 @@ def onset_strength(y=None, sr=22050, S=None, detrend=False, feature=librosa.feat
           at different frequency bins.
 
       - kwargs  
-          Parameters to ``feature()``, if S is not provided.
+          Parameters to ``feature()``, if ``S`` is not provided.
 
-    .. note:: if S is provided, then (y, sr) are optional.
+    .. note:: if ``S`` is provided, then ``(y, sr)`` are optional.
 
     :returns:
       - onsets   : np.ndarray 
-          vector of onset strength
+          vector of onset strengths
 
     :raises:
       - ValueError 
-          if neither (y, sr) nor S are provided
+          if neither ``(y, sr)`` nor ``S`` are provided
 
     """
 
