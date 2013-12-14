148fee4b199e75744eb47b12af90a5ebc5480e02
Brian McFee <brm2132@columbia.edu>
2013-12-13 23:08:20 -0500

    doc theming

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ html_theme = 'bootstrap'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {'bootswatch_theme': 'flatly', 'bootstrap_version': "3"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
