729f6ffd6d88a187e990130b01a99bb1f534c755
Brian McFee <brm2132@columbia.edu>
2013-12-23 14:48:35 -0500

    adding file walker

--- librosa/util.py
+++ librosa/util.py
@@ -50,5 +50,90 @@ def axis_sort(X, axis=-1, value=np.argmax):
     
     return X[:, idx]
 
-
-
+def get_audio_files(directory, ext=None, recurse=True, case_sensitive=False, limit=None, offset=0):
+    '''Get a sorted list of audio files in a directory or directory sub-tree.
+    
+    :usage:
+       >>> # Get all audio files in a directory sub-tree
+       >>> files = librosa.util.get_audio_files('~/Music')
+       
+       >>> # Look only within a specific directory, not the sub-tree
+       >>> files = librosa.util.get_audio_files('~/Music', recurse=False)
+       
+       >>> # Only look for mp3 files
+       >>> files = librosa.util.get_audio_files('~/Music', ext='mp3')
+       
+       >>> # Or just mp3 and ogg
+       >>> files = librosa.util.get_audio_files('~/Music', ext=['mp3', 'ogg'])
+       
+       >>> # Only get the first 10 files
+       >>> files = librosa.util.get_audio_files('~/Music', limit=10)
+       
+       >>> # Or last 10 files
+       >>> files = librosa.util.get_audio_files('~/Music', offset=-10)
+       
+    :parameters:
+      - directory : str
+        Path to look for files
+        
+      - ext : str or list of str
+        A file extension or list of file extensions to include in the search.
+        Default: `['aac', 'au', 'flac', 'm4a', 'mp3', 'ogg', 'wav']`
+        
+      - recurse : boolean
+        If `True`, then all subfolders of `directory` will be searched.
+        Otherwise, only `directory` will be searched.
+        
+      - case_sensitive : boolean
+        If `False`, files matching upper-case version of extensions will be included.
+        
+      - limit : int >0 or None
+        Return at most `limit` files. If `None`, all files are returned.
+        
+      - offset : int
+        Return files starting at `offset` within the list.
+        Use negative values to offset from the end of the list.
+        
+    :returns:
+      - files, list of str
+        The list of audio files.
+    '''
+    
+    def _get_files(D, E):
+        # Expand out the directory
+        D = os.path.abspath(os.path.expanduser(D))
+        
+        myfiles = []
+        for sub_ext in E:
+            globstr = os.path.join(D, '*' + os.path.extsep + sub_ext)
+            myfiles.extend(glob.glob(globstr))
+        
+        return myfiles
+            
+    if ext is None:
+        ext = ['aac', 'au', 'flac', 'm4a', 'mp3', 'ogg', 'wav']
+        
+    elif isinstance(ext, str):
+        if not case_sensitive:
+            ext = ext.lower()
+        ext = [ext]
+        
+    # Generate upper-case versions
+    if not case_sensitive:
+        for i in range(len(ext)):
+            ext.append(ext[i].upper())
+    
+    files = []
+    
+    if recurse:
+        for root, ldirs, lfiles in os.walk(directory):
+            files.extend(_get_files(root, ext))
+    else:
+        files = _get_files(directory, ext)
+    
+    files.sort()
+    files = files[offset:]
+    if limit is not None:
+        files = files[:limit]
+    
+    return files
