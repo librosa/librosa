5917ce962c4516a1d93f01ca27d7e4b89234bc69
mattmcvicar <mattmcvicar@hotmail.com>
2014-01-12 13:43:11 +0900

    added a demo in examples/chord_estimation.py

---
+++ examples/chord_estimation.py
@@ -0,0 +1,10 @@
+"""
+
+This script is to demo chords.py
+
+"""
+
+# Set audio and GT directories
+audio_dir = '/Users/mattmcvicar/Desktop/Work/LibROSA_chords/small_GT'
+GT_dir = '/Users/mattmcvicar/Desktop/Work/LibROSA_chords/small_audio'
+
