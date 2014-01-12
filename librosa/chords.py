14a6c88d4ea3ce9b3f224b70cf8be32799cb48bd
mattmcvicar <mattmcvicar@hotmail.com>
2014-01-12 15:44:52 +0900

    Added chords.read_chords and chords.sample_chords_beat

--- librosa/chords.py
+++ librosa/chords.py
@@ -162,14 +162,13 @@ def train_model( audio_dir, GT_dir, output_feature_dir, output_model_dir ):
     print '  Extracting chroma for ' + full_audio_path
 
     chroma, beat_times = extract_training_chroma( full_audio_path )
-    print chroma.shape
-    print len(beat_times)
-    dfsfsd
 
-  # For each audio/GT pair:
-  #   extract beats
-  #   extract chroma
-  #   assign a chord label to each chroma frame
+    # read chord file
+    chords, chord_start_end = read_chords( os.path.join( GT_dir, gt ) )
+
+    # beat-synch the chroma and chords
+    sampled_chords = sample_chords_beat( chords, chord_start_end, beat_times, no_chord='N' )
+
   # Train model
   # pickle output
 
@@ -178,7 +177,7 @@ def train_model( audio_dir, GT_dir, output_feature_dir, output_model_dir ):
 def extract_training_chroma( audio_file, beat_nfft=4096, beat_hop=64, chroma_nfft=8192, chroma_hop=2048 ):
 
   # load audio
-  y, sr = librosa.load( audio_file )
+  y, sr = librosa.load( audio_file, duration=15.0 )
 
   # track beats
   bpm, beat_frames = librosa.beat.beat_track(y=y, sr=sr, n_fft=beat_nfft, 
@@ -256,10 +255,135 @@ def extract_training_chroma( audio_file, beat_nfft=4096, beat_hop=64, chroma_nff
 
   return Chroma, beat_times
 
-def sample_chords_beat( chords, chord_times, beat_times, no_chord ):
+def read_chords( chord_file ):
 
-  # copy
-  return None  
+  chord_data = open( chord_file ).readlines()
+
+  # sometimes reads an empty line
+  chord_data = [c for c in chord_data if c != '\n']
+
+  n_samples = len( chord_data )
+
+  # Prepare annotations
+  annotation_sample_times = np.zeros( ( n_samples, 2 ) )
+  annotations = []
+
+  for iline, line in enumerate( chord_data ):
+
+    line_data = line.strip().split()
+
+    annotations.append( line_data[ 2 ] )
+    annotation_sample_times[ iline, 0 ] = float( line_data[ 0 ] )
+    annotation_sample_times[ iline, 1 ] = float( line_data[ 1 ] )
+   
+  return annotations, annotation_sample_times
+
+def sample_chords_beat(annotations, annotation_sample_times, sample_times, no_chord='N'):
+
+  # 1. Initialisation 
+  number_samples = annotation_sample_times.shape[ 0 ]
+
+  number_windows = len(sample_times)
+
+  sampled = [ None ] * ( number_windows + 1 ) # Store the annotations of a song
+
+  t_anns = 1           # which label we're at in the unsampled anns
+  t_prev_anns = 1      # 
+  t_sample = 1         # which sampled window (ie the output) we're in
+
+  # 1.1 For the first frame, if it is less than the start time, then no
+  # chord
+  while ( sample_times[ t_sample - 1 ] < annotation_sample_times[ 0, 0 ] ):
+
+    sampled[ t_sample - 1 ] = numStates
+
+    t_sample = t_sample + 1
+
+  # 1.2 Assure that t_sample falls in a chord region
+  while ( annotation_sample_times[ t_anns - 1, 1 ] < sample_times[ t_sample - 1 ] ):
+
+        t_anns = t_anns + 1
+     
+  # 2. go though all time grid
+  while ( t_sample <= number_windows and t_anns <= number_samples ):
+
+    # 2.1 If TS(ts)<TA(ta), then this frame falls in this chord region
+    if ( sample_times[ t_sample - 1 ] <= annotation_sample_times[ t_anns - 1, 1 ] ):
+
+      # A. if the interval between two beats has more than 1 chord
+      if ( t_prev_anns < t_anns ):
+
+        # Majority vote
+        countInterval = 1     
+
+        intervalC = [ 0 ] * ( t_anns - t_prev_anns + 1 )
+                
+        # First interval
+        if t_sample == 1:
+
+          intervalC[ countInterval - 1 ] = annotation_sample_times[ t_prev_anns - 1, 1 ] - annotation_sample_times[ t_prev_anns - 1, 0 ]
+
+        else:
+
+          intervalC[ countInterval - 1 ] = annotation_sample_times[ t_prev_anns - 1, 1 ] - sample_times[ t_sample - 1 - 1 ]
+                
+        countInterval = countInterval + 1
+                
+        # Between intervals
+        for j in range(t_prev_anns+1,t_anns):                      
+          intervalC[countInterval - 1] = annotation_sample_times[ j - 1, 1 ] - annotation_sample_times[ j - 1, 0 ]                        
+          countInterval = countInterval + 1                   
+                
+        # Last interval
+        intervalC[countInterval - 1] = sample_times[t_sample - 1] - annotation_sample_times[ t_anns - 1, 0 ]     
+                
+        # Majority vote
+        maxIndex = np.argmax( intervalC )
+
+        #sampled[ t_sample - 1 ] = annotations[ t_prev_anns - 1 - 1 + maxIndex ]
+        sampled[ t_sample - 1 ] = annotations[ t_prev_anns - 1 + maxIndex ]
+
+        t_prev_anns = t_anns
+        
+      # B. if the interval between two beats falls in 1 chord
+      else:
+
+        sampled[t_sample - 1] = annotations[t_anns - 1]
+
+      t_sample=t_sample + 1
+        
+    # 2.2 Else, find the chord interval this beat falls in
+    else:
+      while (t_anns <= number_samples and annotation_sample_times[ t_anns - 1, 1] < sample_times[t_sample - 1]):
+        t_anns = t_anns + 1
+         
+  # 3. if there are still samples left, assign no chord
+  if t_sample <= number_windows:
+    sampled[ t_sample-1 : ] = [ numStates ] * ( len( sampled ) - t_sample + 1 )
+      
+  if (t_anns == number_samples): # The last chord after final beats
+
+    sampled[number_windows] = annotations[t_anns - 1]
+  
+  elif t_anns < number_samples:
+  
+    countInterval = 1                    
+    intervalC = [ 0 ] * ( number_samples - t_anns + 1 )
+    
+    # Majority vote
+    intervalC[ countInterval - 1 ] = annotation_sample_times[ t_anns - 1, 1 ] - sample_times[ number_windows - 1 ]
+    countInterval = countInterval + 1
+    
+    for j in range(t_anns + 1, number_samples + 1):
+
+      intervalC[countInterval - 1] = annotation_sample_times[j - 1, 1] - annotation_sample_times[j - 1, 0]
+      countInterval = countInterval + 1
+
+    maxIndex = np.argmax(intervalC)
+    sampled[number_windows] = annotations[t_anns - 1 + maxIndex]
+
+  # 4. return the annotation samples
+  return sampled
 
 def train_hidden( chords, n_chords ):
 
