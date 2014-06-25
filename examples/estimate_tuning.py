7c92f1c03da2595931e492b8d6ed23bc0de4434f
Brian McFee <brm2132@columbia.edu>
2014-06-25 16:29:52 -0400

    pep8 examples

--- examples/estimate_tuning.py
+++ examples/estimate_tuning.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-'''
-CREATED:2013-12-09 00:02:54 by Brian McFee <brm2132@columbia.edu>
- 
+'''CREATED:2013-12-09 00:02:54 by Brian McFee <brm2132@columbia.edu>
+
 Estimate the tuning (deviation from A440) of a recording.
 
 Usage: ./tuning.py [-h] input_file
@@ -11,7 +10,10 @@ import argparse
 import sys
 import librosa
 
+
 def estimate_tuning(input_file):
+    '''Load an audio file and estimate tuning (in cents)'''
+
     print 'Loading ', input_file
     y, sr = librosa.load(input_file)
 
@@ -24,17 +26,19 @@ def estimate_tuning(input_file):
 
     print '%+0.2f cents' % (100 * tuning)
 
+
 def process_arguments():
     '''Argparse function to get the program parameters'''
 
-    parser = argparse.ArgumentParser(description='librosa tuning estimation example')
+    parser = argparse.ArgumentParser(description='Tuning estimation example')
 
-    parser.add_argument(    'input_file',
-                            action      =   'store',
-                            help        =   'path to the input file (wav, mp3, etc)')
+    parser.add_argument('input_file',
+                        action='store',
+                        help='path to the input file (wav, mp3, etc)')
 
     return vars(parser.parse_args(sys.argv[1:]))
-   
+
+
 if __name__ == '__main__':
     # Get the parameters
     parameters = process_arguments()
