4fb72b33839460d81db82f1c0400fa4339dcef04
Brian McFee <brian.mcfee@nyu.edu>
2014-11-05 15:37:40 -0500

    updated version number

--- librosa/__init__.py
+++ librosa/__init__.py
@@ -20,4 +20,4 @@ from . import util
 # Exporting all core functions is okay here: suppress the import warning
 from librosa.core import *  # pylint: disable=wildcard-import
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
