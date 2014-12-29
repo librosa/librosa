1c522b8108be1fd4d3628bc52fa224c7c9350af5
Brian McFee <brian.mcfee@nyu.edu>
2014-12-29 11:13:30 -0500

    added tests to frame

--- tests/test_failures.py
+++ tests/test_failures.py
@@ -17,33 +17,39 @@ from nose.tools import raises
 @raises(ValueError)
 def test_mono_valid_stereo():
     '''valid_audio: mono=True,  y.ndim==2'''
-    y = np.random.randn(2, 1000)
+    y = np.zeros((2, 1000))
     librosa.util.valid_audio(y, mono=True)
 
 
-def test_valid_stereo():
-    '''valid_audio: mono=False, y.ndim==2'''
-    y = np.random.randn(2, 1000)
+def test_valid_stereo_or_mono():
+    '''valid_audio: mono=False, y.ndim==1'''
+    y = np.zeros(1000)
     librosa.util.valid_audio(y, mono=False)
 
 
 def test_valid_mono():
     '''valid_audio: mono=True,  y.ndim==1'''
-    y = np.random.randn(1000)
+    y = np.zeros(1000)
     librosa.util.valid_audio(y, mono=True)
+
+
+def test_valid_stereo():
+    '''valid_audio: mono=False, y.ndim==2'''
+    y = np.zeros((2, 1000))
+    librosa.util.valid_audio(y, mono=False)
 
 
 @raises(ValueError)
 def test_valid_audio_type():
     '''valid_audio: list input'''
-    y = list(np.random.randn(1000))
+    y = list(np.zeros(1000))
     librosa.util.valid_audio(y)
 
 
 @raises(ValueError)
 def test_valid_audio_nan():
     '''valid_audio: NaN'''
-    y = np.random.randn(1000)
+    y = np.zeros(1000)
     y[10] = np.NaN
     librosa.util.valid_audio(y)
 
@@ -51,7 +57,33 @@ def test_valid_audio_nan():
 @raises(ValueError)
 def test_valid_audio_inf():
     '''valid_audio: Inf'''
-    y = np.random.randn(1000)
+    y = np.zeros(1000)
     y[10] = np.inf
     librosa.util.valid_audio(y)
 
+
+@raises(ValueError)
+def test_frame_hop():
+    '''frame: hop_length=0'''
+    y = np.zeros(128)
+    librosa.util.frame(y, frame_length=10, hop_length=0)
+
+
+@raises(ValueError)
+def test_frame_discontiguous():
+    '''frame: discontiguous input'''
+    y = np.zeros((128, 2)).T
+    librosa.util.frame(y[0], frame_length=64, hop_length=64)
+
+
+def test_frame_contiguous():
+    '''frame: discontiguous input'''
+    y = np.zeros((2, 128))
+    librosa.util.frame(y[0], frame_length=64, hop_length=64)
+
+
+@raises(ValueError)
+def test_frame_size():
+    '''frame: len(y) == 128, frame_length==256, hop_length=128'''
+    y = np.zeros(64)
+    librosa.util.frame(y, frame_length=256, hop_length=128)
