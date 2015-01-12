9f90b2facab23b705a7d362d3e853f47de1c7b4e
Brian McFee <brian.mcfee@nyu.edu>
2015-01-12 13:43:16 -0500

    fixed doc compilation for feature extractor

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ sys.path.insert(0, os.path.abspath('../librosa'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'numpydoc']
+numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
