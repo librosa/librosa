6ac664a0abc618163f033f3877b53c7f72a96604
Brian McFee <brian.mcfee@nyu.edu>
2015-01-15 12:29:47 -0500

    cqt refactor for #141

--- librosa/core/__init__.py
+++ librosa/core/__init__.py
@@ -5,3 +5,4 @@
 from .time_frequency import *  # pylint: disable=wildcard-import
 from .audio import *  # pylint: disable=wildcard-import
 from .spectrum import *  # pylint: disable=wildcard-import
+from .cqt import * # pylint: disable=wildcard-import
