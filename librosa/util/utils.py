1cbd5a8d6277a0fbd9475a145aeacab8ca0a5caf
Brian McFee <brian.mcfee@nyu.edu>
2015-01-18 15:42:28 -0500

    Cleanup and documentation in util.sparsify_rows

--- librosa/util/utils.py
+++ librosa/util/utils.py
@@ -817,35 +817,92 @@ def peak_pick(x, pre_max, post_max, pre_avg, post_avg, delta, wait):
     return np.array(peaks)
 
 
-# TODO:   2015-01-16 10:05:10 by Brian McFee <brian.mcfee@nyu.edu>
-# write docstrings here 
-# add axis parameter
-# simplify code
 @cache
-def sparsify(fft_basis, sparse_limit=0.01):
+def sparsify_rows(x, quantile=0.01):
     '''
-    Return a sparse version of input fft_basis.
+    Return a row-sparse matrix approximating the input `x`.
 
-    `sparse_limit` is the percentage of the original magnitude sum that can
-    be zeroed out for each filter.
+    Examples
+    --------
+    >>> # Construct a Hann window to sparsify
+    >>> x = scipy.signal.hann(32)
+    >>> x
+    array([ 0.   ,  0.01 ,  0.041,  0.09 ,  0.156,  0.236,  0.326,  0.424,
+            0.525,  0.625,  0.72 ,  0.806,  0.879,  0.937,  0.977,  0.997,
+            0.997,  0.977,  0.937,  0.879,  0.806,  0.72 ,  0.625,  0.525,
+            0.424,  0.326,  0.236,  0.156,  0.09 ,  0.041,  0.01 ,  0.   ])
+    >>> # Discard the bottom percentile
+    >>> x_sparse = librosa.util.sparsify_rows(x, quantile=0.01)
+    >>> x_sparse
+    <1x32 sparse matrix of type '<type 'numpy.float64'>'
+        with 26 stored elements in Compressed Sparse Row format>
+    >>> x_sparse.todense()
+    matrix([[ 0.   ,  0.   ,  0.   ,  0.09 ,  0.156,  0.236,  0.326,
+              0.424,  0.525,  0.625,  0.72 ,  0.806,  0.879,  0.937,
+              0.977,  0.997,  0.997,  0.977,  0.937,  0.879,  0.806,
+              0.72 ,  0.625,  0.525,  0.424,  0.326,  0.236,  0.156,
+              0.09 ,  0.   ,  0.   ,  0.   ]])
+    >>> # Discard up to the bottom 10th percentile
+    >>> x_sparse = librosa.util.sparsify_rows(x, quantile=0.1)
+    >>> x_sparse
+    <1x32 sparse matrix of type '<type 'numpy.float64'>'
+        with 20 stored elements in Compressed Sparse Row format>
+    >>> x_sparse.todense()
+    matrix([[ 0.   ,  0.   ,  0.   ,  0.   ,  0.   ,  0.   ,  0.326,
+              0.424,  0.525,  0.625,  0.72 ,  0.806,  0.879,  0.937,
+              0.977,  0.997,  0.997,  0.977,  0.937,  0.879,  0.806,
+              0.72 ,  0.625,  0.525,  0.424,  0.326,  0.   ,  0.   ,
+              0.   ,  0.   ,  0.   ,  0.   ]])
+
+    Parameters
+    ----------
+    x : np.ndarray [ndim <= 2]
+        The input matrix to sparsify.
+
+    quantile : float in [0, 1.0)
+        Percentage of magnitude to discard in each row of `x`
+
+    Returns
+    -------
+    x_sparse : scipy.sparse.csr_matrix [shape=x.shape]
+        Row-sparsified approximation of `x`
+
+        If `x.ndim == 1`, then `x` is interpreted as a row vector,
+        and `x_sparse.shape == (1, len(x))`.
+
+    Raises
+    ------
+    ValueError
+        If `x.ndim > 2`
+
+        If `quantile` lies outside `[0, 1.0)`
     '''
-    fft_basis = fft_basis.copy()
 
-    if sparse_limit > 0.0:
-        for i, kern in enumerate(fft_basis):
+    if x.ndim == 1:
+        x = x.reshape((1, -1))
 
-            kern_mag = np.abs(kern)
-            band_sum = np.sum(kern_mag)
-            sorted_weights = np.sort(kern_mag)[::-1]
-            cumpct = np.cumsum(sorted_weights)/band_sum
-            cutoff_ind = np.where(cumpct >= (1-sparse_limit))[0][0]
-            cutoff_val = sorted_weights[cutoff_ind]
-            zero_inds = np.where(kern_mag < cutoff_val)
-            fft_basis[i][zero_inds] = 0.0
+    elif x.ndim > 2:
+        raise ValueError('Input must have 2 or fewer dimensions.  '
+                         'Provided x.shape={:s}.'.format(x.shape))
 
-        fft_basis = scipy.sparse.csr_matrix(fft_basis)
+    if not (0.0 <= quantile < 1):
+        raise ValueError('Invalid quantile: {:.2f}'.format(quantile))
 
-    return fft_basis
+    x_sparse = scipy.sparse.lil_matrix(x.shape, dtype=x.dtype)
+
+    mags = np.abs(x)
+    norms = np.sum(mags, axis=1, keepdims=True)
+
+    mag_sort = np.sort(mags, axis=1)
+    cumulative_mag = np.cumsum(mag_sort / norms, axis=1)
+
+    threshold_idx = np.argmin(cumulative_mag < quantile, axis=1)
+
+    for i, j in enumerate(threshold_idx):
+        idx = np.where(mags[i] >= mag_sort[i, j])
+        x_sparse[i, idx] = x[i, idx]
+
+    return x_sparse.tocsr()
 
 
 def find_files(directory, ext=None, recurse=True, case_sensitive=False,
