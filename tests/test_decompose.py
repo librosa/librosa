8c2e67f17154de4fa19097e364a42b4e943732fc
Brian McFee <brian.mcfee@nyu.edu>
2015-01-20 09:51:26 -0500

    100% coverage of decompose module. #150

--- tests/test_decompose.py
+++ tests/test_decompose.py
@@ -11,6 +11,7 @@ except:
 
 import numpy as np
 import librosa
+import sklearn.decomposition
 
 
 def test_default_decompose():
@@ -20,3 +21,56 @@ def test_default_decompose():
     (W, H) = librosa.decompose.decompose(X)
 
     assert np.allclose(X, W.dot(H), rtol=1e-2, atol=1e-2)
+
+
+def test_given_decompose():
+
+    D = sklearn.decomposition.NMF()
+
+    X = np.array([[1, 2, 3, 4, 5, 6], [1, 1, 1.2, 1, 0.8, 1]])
+    
+    (W, H) = librosa.decompose.decompose(X, transformer=D)
+
+    assert np.allclose(X, W.dot(H), rtol=1e-2, atol=1e-2)
+
+
+def test_sorted_decompose():
+
+    X = np.array([[1, 2, 3, 4, 5, 6], [1, 1, 1.2, 1, 0.8, 1]])
+    
+    (W, H) = librosa.decompose.decompose(X, sort=True)
+
+    assert np.allclose(X, W.dot(H), rtol=1e-2, atol=1e-2)
+
+
+def test_real_hpss():
+
+    # Load an audio signal
+    y, sr = librosa.load('data/test1_22050.wav')
+
+    D = np.abs(librosa.stft(y))
+
+    def __hpss_test(w, p, m):
+        H, P = librosa.decompose.hpss(D, kernel_size=w, power=p, mask=m)
+
+        if m:
+            assert np.allclose(H + P, np.ones_like(D))
+        else:
+            assert np.allclose(H + P, D)
+
+    for window in [31, (5, 5)]:
+        for power in [0, 1, 2]:
+            for mask in [False, True]:
+                yield __hpss_test, window, power, mask
+
+
+def test_complex_hpss():
+
+    # Load an audio signal
+    y, sr = librosa.load('data/test1_22050.wav')
+
+    D = librosa.stft(y)
+
+    H, P = librosa.decompose.hpss(D)
+
+    assert np.allclose(H + P, D)
