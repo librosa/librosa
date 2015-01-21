0ca728731b7dba8a15b9f404406d3fdb101c8042
Brian McFee <brian.mcfee@nyu.edu>
2015-01-21 13:55:08 -0500

    adding tests for segmentation band masks #150

---
+++ tests/test_segment.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''Tests for segmentation functions'''
+
+# Disable cache
+import os
+try:
+    os.environ.pop('LIBROSA_CACHE_DIR')
+except:
+    pass
+
+import librosa
+import numpy as np
+
+
+def test_band_infinite():
+
+    def __test(width, n, v_in, v_out):
+        B = librosa.segment.__band_infinite(n, width, v_in=v_in, v_out=v_out)
+
+        idx = np.tril_indices(n, k=width-1)
+
+        # First check the shape
+        assert np.allclose(B.shape, [n, n])
+
+        # Check symmetry
+        assert np.allclose(B, B.T)
+
+        # Check in-band first
+        Bincheck = B.copy()
+        Bincheck[idx] = v_in
+        Bincheck.T[idx] = v_in
+
+        assert np.all(Bincheck == v_in)
+
+        # If we're too small to have an out-of-band, then we're done
+        if not len(idx[0]):
+            return
+
+        Boutcheck = B.copy()
+        Boutcheck[idx] = v_out
+        Boutcheck.T[idx] = v_out
+
+        assert np.all(Boutcheck == v_out)
+
+    for width in [0, 1, 2, 3, 5, 9]:
+        for n in [width, width+1, width * 2, width*width]:
+            
+            yield __test, width, n, -1, +1
+
+
+def test_recurrence_matrix():
+
+    def __test(n, k, width, sym):
+        # Make a data matrix
+        data = np.random.randn(3, n)
+
+        D = librosa.segment.recurrence_matrix(data, k=k, width=width, sym=sym)
+
+        # First test for symmetry
+        if sym:
+            assert np.allclose(D, D.T)
+
+        # If not symmetric, test for correct number of links
+        if not sym and k is not None:
+            real_k = min(k, n - width)
+            assert not np.any(D.sum(axis=1) != real_k)
+
+        # Make sure the +- width diagonal is hollow
+        # It's easier to test if zeroing out the triangles leaves nothing
+        idx = np.tril_indices(n, k=width)
+
+        D[idx] = False
+        D.T[idx] = False
+        assert not np.any(D)
+
+    for n in [10, 100, 1000]:
+        for k in [None, int(n/4)]:
+            for width in [1, 3, 5]:
+                for sym in [False, True]:
+                    yield __test, n, k, width, sym
