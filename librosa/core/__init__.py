f9d1a382364120478e2464645c01ea237f52914a
Brian McFee <brian.mcfee@nyu.edu>
2015-01-26 15:23:51 -0500

    adding toc for submodules

--- librosa/core/__init__.py
+++ librosa/core/__init__.py
@@ -1,6 +1,76 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Core IO, DSP and utility functions."""
+"""Core IO, DSP and utility functions.
+
+Audio processing
+================
+.. autosummary::
+    :toctree: generated/
+
+    load
+    to_mono
+    resample
+    get_duration
+    autocorrelate
+    zero_crossings
+
+
+Spectral representations
+========================
+.. autosummary::
+    :toctree: generated/
+
+    stft
+    istft
+    ifgram
+    cqt
+
+    phase_vocoder
+
+    magphase
+    logamplitude
+    perceptual_weighting
+
+Time and frequency conversion
+=============================
+.. autosummary::
+    :toctree: generated/
+
+    frames_to_samples
+    frames_to_time
+    samples_to_frames
+    samples_to_time
+    time_to_frames
+    time_to_samples
+
+    hz_to_note
+    hz_to_midi
+    midi_to_hz
+    midi_to_note
+    note_to_hz
+    note_to_midi
+
+    hz_to_mel
+    hz_to_octs
+    mel_to_hz
+    octs_to_hz
+
+    fft_frequencies
+    cqt_frequencies
+    mel_frequencies
+
+
+Pitch and tuning
+================
+.. autosummary::
+    :toctree: generated/
+
+    estimate_tuning
+    pitch_tuning
+    piptrack
+    ifptrack
+
+"""
 
 from .time_frequency import *  # pylint: disable=wildcard-import
 from .audio import *  # pylint: disable=wildcard-import
