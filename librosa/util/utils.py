fixed local max calculation in peak_pick #161

librosa/util/utils.py
@@ -839,7 +839,8 @@ def peak_pick(x, pre_max, post_max, pre_avg, post_avg, delta, wait):
     max_length = pre_max + post_max
     max_origin = 0.5 * (pre_max - post_max)
     mov_max = scipy.ndimage.filters.maximum_filter1d(x, int(max_length),
-                                                     mode='nearest',
+                                                     mode='constant',
+                                                     cval=x.min(),
                                                      origin=int(max_origin))
 
     # Get the mean of the signal over a sliding window