35d3db904463af45b534feef5cd4d2e7e8a9e9e2
Brian McFee <brian.mcfee@nyu.edu>
2015-02-15 13:00:46 -0500

    fixed a bug in tuning test

--- tests/test_core.py
+++ tests/test_core.py
@@ -391,7 +391,7 @@ def test_estimate_tuning():
 
         for resolution in [1e-2]:
             for bins_per_octave in [12]:
-
+                for center_note in [69, 84, 108]:
                     for tuning in [-0.5, -0.375, -0.25, -0.125, 0.0, 0.125, 0.25, 0.375]:
                         target_hz = librosa.midi_to_hz(center_note + tuning)
 
