b298201013e052b3197541d5084c41fbabce593b
Brian McFee <brian.mcfee@nyu.edu>
2015-02-15 17:53:27 -0500

    added tests for spectrogram input onset strength calculation

--- tests/test_onset.py
+++ tests/test_onset.py
@@ -62,3 +62,46 @@ def test_onset_strength_audio():
                             tf = raises(ValueError)(__test)
                             yield (tf, None, sr, feature, n_fft,
                                    hop_length, detrend, centering)
+
+
+def test_onset_strength_spectrogram():
+
+    def __test(S, sr, feature, n_fft, hop_length, detrend, centering):
+
+        oenv = librosa.onset.onset_strength(y=None, sr=sr,
+                                            S=S,
+                                            detrend=detrend,
+                                            centering=centering,
+                                            aggregate=aggregate,
+                                            feature=feature,
+                                            n_fft=n_fft,
+                                            hop_length=hop_length)
+
+        assert oenv.ndim == 1
+
+        target_shape = S.shape[-1]
+
+        if centering:
+            target_shape += n_fft // (2 * hop_length)
+
+        if not detrend:
+            assert np.all(oenv >= 0)
+
+        eq_(oenv.shape[-1], target_shape)
+
+    y, sr = librosa.load(__EXAMPLE_FILE)
+    S = librosa.feature.melspectrogram(y=y, sr=sr)
+
+    for feature in [None,
+                    librosa.feature.melspectrogram,
+                    librosa.feature.chromagram]:
+        for n_fft in [512, 2048]:
+            for hop_length in [n_fft // 2, n_fft // 4]:
+                for detrend in [False, True]:
+                    for centering in [False, True]:
+                        for aggregate in [None, np.mean, np.max]:
+                            yield (__test, S, sr, feature, n_fft,
+                                   hop_length, detrend, centering)
+                            tf = raises(ValueError)(__test)
+                            yield (tf, None, sr, feature, n_fft,
+                                   hop_length, detrend, centering)
