2ed7b9a1b229caeb10aee5c791db4e6adf5dd877
Brian McFee <brian.mcfee@nyu.edu>
2015-04-03 11:44:31 -0400

    simplified some dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     keywords='audio music sound',
     license='ISC',
