87861d865f8dcd1964448b1b926366fc870d8df3
Brian McFee <brian.mcfee@nyu.edu>
2015-06-06 10:18:47 -0400

    docstring updates

--- librosa/feature/spectral.py
+++ librosa/feature/spectral.py
@@ -952,7 +952,7 @@ def tonnetz(y=None, sr=22050, chroma=None):
 
     Returns
     -------
-    ton : np.ndarray [shape(6, t)]
+    tonnetz : np.ndarray [shape(6, t)]
         Tonal centroid features for each frame.
 
         Tonnetz dimensions:
@@ -966,14 +966,17 @@ def tonnetz(y=None, sr=22050, chroma=None):
     See Also
     --------
     chroma_cqt
-        Compute a chromagram from a constat-Q transform.
+        Compute a chromagram from a constant-Q transform.
 
     chroma_stft
         Compute a chromagram from an STFT spectrogram or waveform.
 
     Examples
     --------
+    Compute tonnetz features from the harmonic component of a song
+
     >>> y, sr = librosa.load(librosa.util.example_audio_file())
+    >>> y = librosa.effects.harmonic(y)
     >>> tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
     >>> tonnetz
     array([[-0.073, -0.053, ..., -0.054, -0.073],
@@ -982,10 +985,18 @@ def tonnetz(y=None, sr=22050, chroma=None):
            [ 0.039,  0.034, ...,  0.044,  0.064],
            [ 0.005,  0.002, ...,  0.011,  0.017]])
 
+    Compare the tonnetz features to `chroma_cqt`
+
     >>> import matplotlib.pyplot as plt
-    >>> librosa.display.specshow(tonnetz, y_axis='tonnetz', x_axis='time')
+    >>> plt.subplot(2, 1, 1)
+    >>> librosa.display.specshow(tonnetz, y_axis='tonnetz')
     >>> plt.colorbar()
     >>> plt.title('Tonal Centroids (Tonnetz)')
+    >>> plt.subplot(2, 1, 2)
+    >>> librosa.display.specshow(librosa.feature.chroma_cqt(y, sr=sr),
+    ...                          y_axis='chroma', x_axis='time')
+    >>> plt.colorbar()
+    >>> plt.title('Chroma')
     >>> plt.tight_layout()
 
     '''
