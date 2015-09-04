2fb8a3b72ed4a9a7789da49ef1bfb4820fe57470
Brian McFee <brian.mcfee@nyu.edu>
2015-09-04 09:59:11 -0400

    extracted magic numbers from tempogram example

--- librosa/feature/rhythm.py
+++ librosa/feature/rhythm.py
@@ -79,13 +79,16 @@ def tempogram(y=None, sr=22050, onset_envelope=None, hop_length=512,
     --------
     >>> # Compute local onset autocorrelation
     >>> y, sr = librosa.load(librosa.util.example_audio_file())
-    >>> oenv = librosa.onset.onset_strength(y=y, sr=sr, centering=False)
-    >>> tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr)
+    >>> hop_length = 512
+    >>> oenv = librosa.onset.onset_strength(y=y, sr=sr, hop_length=hop_length,
+    ...                                     centering=False)
+    >>> tempogram = librosa.feature.tempogram(onset_envelope=oenv, sr=sr,
+    ...                                       hop_length=hop_length)
     >>> # Compute global onset autocorrelation
     >>> ac_global = librosa.autocorrelate(oenv, max_size=tempogram.shape[0])
     >>> ac_global = librosa.util.normalize(ac_global)
     >>> # Estimate the global tempo for display purposes
-    >>> tempo = librosa.beat.estimate_tempo(oenv, sr=sr)
+    >>> tempo = librosa.beat.estimate_tempo(oenv, sr=sr, hop_length=hop_length)
 
     >>> import matplotlib.pyplot as plt
     >>> plt.figure(figsize=(8, 6))
@@ -96,10 +99,11 @@ def tempogram(y=None, sr=22050, onset_envelope=None, hop_length=512,
     >>> plt.axis('tight')
     >>> plt.subplot(3, 1, 2)
     >>> # We'll truncate the display to a narrower range of tempi
-    >>> librosa.display.specshow(tempogram[:96], x_axis='time', y_axis='tempo',
+    >>> librosa.display.specshow(tempogram[:100], sr=sr, hop_length=hop_length,
+    >>>                          x_axis='time', y_axis='tempo',
     ...                          tmin=tempo/4, tmax=2*tempo, n_yticks=4)
     >>> plt.subplot(3, 1, 3)
-    >>> x = np.linspace(0, tempogram.shape[0] * 512. / sr, num=tempogram.shape[0])
+    >>> x = np.linspace(0, tempogram.shape[0] * float(hop_length) / sr, num=tempogram.shape[0])
     >>> plt.plot(x, np.mean(tempogram, axis=1), label='Mean local autocorrelation')
     >>> plt.plot(x, ac_global, '--', alpha=0.75, label='Global autocorrelation')
     >>> plt.xlabel('Lag (seconds)')
