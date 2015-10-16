c79b1dbc44f6444b60eee2c9c962688f28a9c08e
Brian McFee <brian.mcfee@nyu.edu>
2015-10-16 14:47:51 -0400

    added python 3.5 to classifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     keywords='audio music sound',
     license='ISC',
