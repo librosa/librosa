2419f288436da701884597f2fc1f2ada2528db31
Stefan Balke <stefan.balke@audiolabs-erlangen.de>
2016-04-06 13:38:22 +0200

    added numba to the requirements

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ setup(
         'joblib >= 0.7.0',
         'decorator >= 3.0.0',
         'six >= 1.3',
-        'resampy >= 0.1.0'
+        'resampy >= 0.1.0',
+        'numba >= 0.25',
     ],
     extras_require={
         'resample': 'scikits.samplerate>=0.3',
