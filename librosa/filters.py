bbb5099c8002641386051cddade5ff9847357fba
Brian McFee <brian.mcfee@nyu.edu>
2016-05-19 10:27:53 -0400

    fixing doc build issues

--- librosa/filters.py
+++ librosa/filters.py
@@ -416,17 +416,17 @@ def constant_q(sr, fmin=None, n_bins=84, bins_per_octave=12, tuning=0.0,
 
     Plot one octave of filters in time and frequency
 
-    >>> basis, lengths = librosa.filters.constant_q(22050)
     >>> import matplotlib.pyplot as plt
+    >>> basis, lengths = librosa.filters.constant_q(22050)
     >>> plt.figure(figsize=(10, 6))
     >>> plt.subplot(2, 1, 1)
     >>> notes = librosa.midi_to_note(np.arange(24, 24 + len(basis)))
-    >>> for i, (f, n) in enumerate(zip(basis, notes)[:12]):
+    >>> for i, (f, n) in enumerate(zip(basis, notes[:12])):
     ...     f_scale = librosa.util.normalize(f) / 2
     ...     plt.plot(i + f_scale.real)
     ...     plt.plot(i + f_scale.imag, linestyle=':')
     >>> plt.axis('tight')
-    >>> plt.yticks(range(len(notes[:12])), notes[:12])
+    >>> plt.yticks(np.arange(len(notes[:12])), notes[:12])
     >>> plt.ylabel('CQ filters')
     >>> plt.title('CQ filters (one octave, time domain)')
     >>> plt.xlabel('Time (samples at 22050 Hz)')
@@ -436,7 +436,7 @@ def constant_q(sr, fmin=None, n_bins=84, bins_per_octave=12, tuning=0.0,
     >>> # Keep only the positive frequencies
     >>> F = F[:, :(1 + F.shape[1] // 2)]
     >>> librosa.display.specshow(F, x_axis='linear')
-    >>> plt.yticks(range(len(notes))[::12], notes[::12])
+    >>> plt.yticks(np.arange(len(notes))[::12], notes[::12])
     >>> plt.ylabel('CQ filters')
     >>> plt.title('CQ filter magnitudes (frequency domain)')
     >>> plt.tight_layout()
