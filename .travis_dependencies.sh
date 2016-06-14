09d4f1dec081422759cac466ab184fd1c41ada1e
Stefan Balke <stefan.balke@audiolabs-erlangen.de>
2016-06-14 15:30:39 +0200

    add numba to the travis deps

--- .travis_dependencies.sh
+++ .travis_dependencies.sh
@@ -11,7 +11,7 @@ conda_create ()
     conda update -q conda
     conda config --add channels pypi
     conda info -a
-    deps='pip numpy scipy pandas requests nose coverage numpydoc matplotlib sphinx scikit-learn seaborn'
+    deps='pip numpy scipy pandas requests nose coverage numpydoc matplotlib sphinx scikit-learn seaborn numba'
 
     conda create -q -n $ENV_NAME "python=$TRAVIS_PYTHON_VERSION" $deps
     conda update --all
