564b847b4eb52cbc28ab9fb3adae094e8c37d24d
Stefan Balke <stefan.balke@audiolabs-erlangen.de>
2016-06-20 15:45:10 +0200

    numba as conda dep

--- .travis_dependencies.sh
+++ .travis_dependencies.sh
@@ -11,7 +11,7 @@ conda_create ()
     conda update -q conda
     conda config --add channels pypi
     conda info -a
-    deps='pip numpy scipy pandas requests nose coverage numpydoc matplotlib sphinx scikit-learn seaborn numba'
+    deps='pip numpy scipy pandas requests nose coverage numpydoc matplotlib sphinx scikit-learn seaborn'
 
     conda create -q -n $ENV_NAME "python=$TRAVIS_PYTHON_VERSION" $deps
     conda update --all
@@ -42,7 +42,8 @@ if [ ! -d "$src" ]; then
 
         pip install git+https://github.com/bmcfee/samplerate.git
         pip install python-coveralls
-            
+        conda install numba
+
         source deactivate
     popd
 else
