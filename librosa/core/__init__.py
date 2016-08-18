861e050728ba4123ca9c67cad64b6c45be35b749
Brian McFee <brian.mcfee@nyu.edu>
2016-08-18 18:57:38 -0400

    refactoring harmonics out to its own subsubmod

--- librosa/core/__init__.py
+++ librosa/core/__init__.py
@@ -96,5 +96,6 @@ from .spectrum import *  # pylint: disable=wildcard-import
 from .pitch import *  # pylint: disable=wildcard-import
 from .constantq import *  # pylint: disable=wildcard-import
 from .dtw import *  # pylint: disable=wildcard-import
+from .harmonic import * # pylint: disable=wildcard-import
 
 __all__ = [_ for _ in dir() if not _.startswith('_')]
