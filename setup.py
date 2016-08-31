f31b5222ba3267f831fad9178ed37f6c891142ce
Carl Thomé <carlthome@gmail.com>
2016-08-31 16:17:31 +0200

    Renamed extras stft to fftw

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
                  'matplotlib >= 1.5'],
         'numba': ['numba >= 0.25'],
         'display': ['matplotlib >= 1.5'],
-        'stft': ['pyfftw']
+        'fftw': ['pyfftw => 0.10.4']
     }
 )
