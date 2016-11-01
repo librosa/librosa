8c518465fc0871d1474509f955fc08805934740e
Brian McFee <brian.mcfee@nyu.edu>
2016-11-01 10:56:54 -0400

    Added effecs.trim
    fixed an index error on trim
    
    Added test coverage for trim
    
    Added reference power option to trim
    
    tightening trim test

--- tests/test_effects.py
+++ tests/test_effects.py
@@ -139,3 +139,46 @@ def test_harmonic():
     yh2 = librosa.effects.harmonic(y)
 
     assert np.allclose(yh1, yh2)
+
+
+def test_trim():
+
+    def __test(y, top_db, ref_power, index):
+
+        if index:
+            yt, idx = librosa.effects.trim(y, top_db=top_db,
+                                           ref_power=ref_power,
+                                           index=True)
+
+            # Test for index position
+            fidx = [slice(None)] * y.ndim
+            fidx[-1] = idx
+            assert np.allclose(yt, y[fidx])
+
+        else:
+            yt = librosa.effects.trim(y, top_db=top_db, ref_power=ref_power,
+                                      index=False)
+
+        # Verify logamp
+        rms = librosa.feature.rmse(librosa.to_mono(yt))
+        logamp = librosa.logamplitude(rms**2, ref_power=ref_power, top_db=None)
+
+        assert np.all(logamp >= - top_db)
+
+        # Verify duration
+        duration = librosa.get_duration(yt)
+        assert np.allclose(duration, 3.0, atol=1e-1), duration
+
+    # construct 5 seconds of stereo silence
+    # Stick a sine wave in the middle three seconds
+    sr = float(22050)
+    y = np.zeros((2, int(5 * sr)))
+    y[0, sr:4*sr] = np.sin(2 * np.pi * 440 * np.arange(0, 3 * sr) / sr)
+
+    for top_db in [60, 40, 20]:
+        for index in [False, True]:
+            for ref_power in [1, np.max]:
+                # Test stereo
+                yield __test, y, top_db, ref_power, index
+                # Test mono
+                yield __test, y[0], top_db, ref_power, index
