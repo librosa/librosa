8d817b20d3291eaeeb42b8404257362aa0d471a9
Brian McFee <brian.mcfee@nyu.edu>
2017-01-07 13:39:00 -0500

    removed warning in beat units tests

--- tests/test_onset.py
+++ tests/test_onset.py
@@ -211,7 +211,7 @@ def test_onset_units():
 
         assert np.allclose(t1, t2)
 
-    for sr in [None, 11024]:
+    for sr in [None, 44100]:
         y, sr = librosa.load(__EXAMPLE_FILE, sr=sr)
         for hop_length in [512, 1024]:
             for units in ['frames', 'time', 'samples']:
