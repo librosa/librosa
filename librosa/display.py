669ef408a8607648d31e41b81c3f93033568b9d6
Brian McFee <brian.mcfee@nyu.edu>
2017-01-10 18:18:16 -0500

    revised figure formatting for mpl2

--- librosa/display.py
+++ librosa/display.py
@@ -191,10 +191,11 @@ class LogHzFormatter(Formatter):
     >>> plt.figure()
     >>> ax1 = plt.subplot(2,1,1)
     >>> ax1.bar(np.arange(len(values)), values)
+    >>> ax1.yaxis.set_major_formatter(librosa.display.LogHzFormatter())
     >>> ax1.set_ylabel('Hz')
     >>> ax2 = plt.subplot(2,1,2)
     >>> ax2.bar(np.arange(len(values)), values)
-    >>> ax2.yaxis.set_major_formatter(librosa.display.LogHzFormatter())
+    >>> ax2.yaxis.set_major_formatter(librosa.display.NoteFormatter())
     >>> ax2.set_ylabel('Note')
     '''
     def __init__(self, major=True):
