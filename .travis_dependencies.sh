206448d20055e883fad87359cdd0b7ca3f981284
Brian McFee <brian.mcfee@nyu.edu>
2017-09-01 14:25:30 -0400

    fixed sklearn dep in travis

--- .travis_dependencies.sh
+++ .travis_dependencies.sh
@@ -11,7 +11,7 @@ conda_create ()
     conda update -q conda
     conda config --add channels pypi
     conda info -a
-    deps='pip numpy scipy nose coverage scikit-learn matplotlib'
+    deps='pip numpy scipy nose coverage scikit-learn!=0.19.0 matplotlib'
 
     conda create -q -n $ENV_NAME "python=$TRAVIS_PYTHON_VERSION" $deps
     conda update --all
