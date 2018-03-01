fa26f01304b2d05706f01265280820576d6716be
Keunwoo Choi <gnuchoi+github@gmail.com>
2018-03-01 16:59:57 +0000

    change mode --> pad_mod in docstrings

--- librosa/core/spectrum.py
+++ librosa/core/spectrum.py
@@ -71,7 +71,7 @@ def stft(y, n_fft=2048, hop_length=None, win_length=None, window='hann',
     dtype       : numeric type
         Complex numeric type for `D`.  Default is 64-bit complex.
 
-    mode : string
+    pad_mode : string
         If `center=True`, the padding mode to use at the edges of the signal.
         By default, STFT uses reflection padding.
 
@@ -391,7 +391,7 @@ def ifgram(y, sr=22050, n_fft=2048, hop_length=None, win_length=None,
     dtype : numeric type
         Complex numeric type for `D`.  Default is 64-bit complex.
 
-    mode : string
+    pad_mode : string
         If `center=True`, the padding mode to use at the edges of the signal.
         By default, STFT uses reflection padding.
 
