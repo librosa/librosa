65414596f7049075b2f61ec7a24187023be4c6bb
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-06-27 16:02:28 -0400

    use magphase before power_to_db in plot_presets
    NB: I renamed M_highres to M_hop512 and M2 to M_11k
    This is because the term "high-resolution" may lead to confusion, as it conflates with the terminology of subspace tracking (ESPRIT algorithm)

--- docs/examples/plot_presets.py
+++ docs/examples/plot_presets.py
@@ -59,29 +59,32 @@ librosa['n_fft'] = 4096
 ######################################################################
 # Now we can load in a file and do some analysis with the new defaults
 filename = 'audio/Karissa_Hobbs_-_09_-_Lets_Go_Fishin.mp3'
-
 y, sr = librosa.load(filename, duration=5, offset=35)
 
-# Generate a Mel spectrogram:
-
+# Generate a complex-valued mel-frequency spectrogram:
 M = librosa.feature.melspectrogram(y=y)
 
-# Of course, you can still override the new default manually, e.g.:
 
-M_highres = librosa.feature.melspectrogram(y=y, hop_length=512)
+# Extract magnitudes and phases from complex time-frequency bins in M:
+M_mag, M_phase = librosa.magphase(M)
+
+
+# Of course, you can still override the new default manually, e.g.:
+M_hop512 = librosa.feature.melspectrogram(y=y, hop_length=512)
+M_hop512_mag = librosa.magphase(M)[0]
 
 
 # And plot the results
 plt.figure(figsize=(6, 6))
 ax = plt.subplot(3, 1, 1)
 
-librosa.display.specshow(librosa.power_to_db(M, ref=np.max),
+librosa.display.specshow(librosa.power_to_db(M_mag, ref=np.max),
                          y_axis='mel', x_axis='time')
 
 plt.title('44100/1024/4096')
 
 plt.subplot(3, 1, 2, sharex=ax, sharey=ax)
-librosa.display.specshow(librosa.power_to_db(M_highres, ref=np.max),
+librosa.display.specshow(librosa.power_to_db(M_hop512_mag, ref=np.max),
                          hop_length=512,
                          y_axis='mel', x_axis='time')
 plt.title('44100/512/4096')
@@ -91,10 +94,11 @@ plt.title('44100/512/4096')
 librosa['sr'] = 11025
 
 y2, sr2 = librosa.load(filename, duration=5, offset=35)
-M2 = librosa.feature.melspectrogram(y=y2, sr=sr2)
+M_11k = librosa.feature.melspectrogram(y=y2, sr=sr2)
+M_11k_mag = librosa.magphase(M_11k)[0]
 
 plt.subplot(3, 1, 3, sharex=ax, sharey=ax)
-librosa.display.specshow(librosa.power_to_db(M2, ref=np.max),
+librosa.display.specshow(librosa.power_to_db(M_11k_mag, ref=np.max),
                          y_axis='mel', x_axis='time')
 
 plt.title('11025/1024/4096')
