78255202c1a79456d17fe06ddd264eb1a8f7ae64
Vincent Lostanlen <vincent.lostanlen@nyu.edu>
2018-07-28 08:36:09 -0400

    variable naming: cqt -> C in cqt docstring

--- librosa/util/utils.py
+++ librosa/util/utils.py
@@ -1305,23 +1305,23 @@ def sync(data, idx, aggregate=None, pad=True, axis=-1):
 
     >>> y, sr = librosa.load(librosa.util.example_audio_file())
     >>> tempo, beats = librosa.beat.beat_track(y=y, sr=sr, trim=False)
-    >>> cqt = np.abs(librosa.cqt(y=y, sr=sr))
-    >>> beats = librosa.util.fix_frames(beats, x_max=cqt.shape[1])
+    >>> C = np.abs(librosa.cqt(y=y, sr=sr))
+    >>> beats = librosa.util.fix_frames(beats, x_max=C.shape[1])
 
     By default, use mean aggregation
 
-    >>> cqt_avg = librosa.util.sync(cqt, beats)
+    >>> C_avg = librosa.util.sync(C, beats)
 
     Use median-aggregation instead of mean
 
-    >>> cqt_med = librosa.util.sync(cqt, beats,
+    >>> C_med = librosa.util.sync(C, beats,
     ...                             aggregate=np.median)
 
     Or sub-beat synchronization
 
-    >>> sub_beats = librosa.segment.subsegment(cqt, beats)
-    >>> sub_beats = librosa.util.fix_frames(sub_beats, x_max=cqt.shape[1])
-    >>> cqt_med_sub = librosa.util.sync(cqt, sub_beats, aggregate=np.median)
+    >>> sub_beats = librosa.segment.subsegment(C, beats)
+    >>> sub_beats = librosa.util.fix_frames(sub_beats, x_max=C.shape[1])
+    >>> C_med_sub = librosa.util.sync(C, sub_beats, aggregate=np.median)
 
 
     Plot the results
@@ -1331,18 +1331,18 @@ def sync(data, idx, aggregate=None, pad=True, axis=-1):
     >>> subbeat_t = librosa.frames_to_time(sub_beats, sr=sr)
     >>> plt.figure()
     >>> plt.subplot(3, 1, 1)
-    >>> librosa.display.specshow(librosa.amplitude_to_db(cqt,
+    >>> librosa.display.specshow(librosa.amplitude_to_db(C,
     ...                                                  ref=np.max),
     ...                          x_axis='time')
-    >>> plt.title('CQT power, shape={}'.format(cqt.shape))
+    >>> plt.title('CQT power, shape={}'.format(C.shape))
     >>> plt.subplot(3, 1, 2)
-    >>> librosa.display.specshow(librosa.amplitude_to_db(cqt_med,
+    >>> librosa.display.specshow(librosa.amplitude_to_db(C_med,
     ...                                                  ref=np.max),
     ...                          x_coords=beat_t, x_axis='time')
     >>> plt.title('Beat synchronous CQT power, '
-    ...           'shape={}'.format(cqt_med.shape))
+    ...           'shape={}'.format(C_med.shape))
     >>> plt.subplot(3, 1, 3)
-    >>> librosa.display.specshow(librosa.amplitude_to_db(cqt_med_sub,
+    >>> librosa.display.specshow(librosa.amplitude_to_db(C_med_sub,
     ...                                                  ref=np.max),
     ...                          x_coords=subbeat_t, x_axis='time')
     >>> plt.title('Sub-beat synchronous CQT power, '
