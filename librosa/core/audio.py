Fix calculation of a, use a random seed with better test

librosa/core/audio.py
@@ -536,27 +536,31 @@ def lpc(y, N):
     if not np.all(np.isreal(y)):
         raise ParameterError("y must not contain complex numbers")
 
-    return _lpc(y, N)
+    return __lpc(y, N)
 
 
 @jit(nopython=True)
-def _lpc(y, N):
+def __lpc(y, N):
     k = np.zeros(N, dtype=y.dtype)
-    a = np.zeros(N+1, dtype=y.dtype)
+    a = np.zeros((2, N+1), dtype=y.dtype)
 
-    q = a[0] = 1
+    q = a[:, 0] = 1
     fp = y[1:]
     bp = y[:-1]
     den = np.dot(fp, fp) + np.dot(bp, bp)
+    cur = 0
 
     for i in range(N):
         # Compute reflection coefficient
         k[i] = -2 * np.dot(bp, fp) / den
         q = 1 - k[i]**2
 
-        # Levinson-Durbin recursion (note we're flipping a)
+        # Levinson-Durbin recursion. We're flipping a while we rewrite it
+        # so we maintain two buffers
+        cur ^= 1
+        last = cur ^ 1
         for j in range(1, i+2):
-            a[j] = a[j] + k[i]*a[i - j + 1]
+            a[cur, j] = a[last, j] + k[i]*a[last, i - j + 1]
 
         # Forward and backward prediction error updates
         fp_tmp = fp + k[i]*bp
@@ -572,7 +576,7 @@ def _lpc(y, N):
         # to speed up computation
         den = q*den - bp[-1]**2 - fp[0]**2
 
-    return a
+    return a[cur, :]
 
 
 @cache(level=20)

tests/test_core.py
@@ -610,15 +610,21 @@ def test_autocorrelate():
             for max_size in [None, y.shape[axis]//2, y.shape[axis], 2 * y.shape[axis]]:
                 yield __test, y, truth[axis], max_size, axis
 
+
 def test_lpc():
     # filter some noise, and estimate the coefficients from
-    # the filtered noise, check equality to a generous
-    # tolerance
-    noise = np.random.randn(1000)
+    # the filtered noise, do a bunch of iterations and check
+    # equality to a generous tolerance
+    srand()
+
+    n = 5000
+    est_a = np.zeros((n, 6))
     truth_a = [1, 0.5, 0.4, 0.3, 0.2, 0.1]
-    filtered = scipy.signal.lfilter([1], truth_a, noise)
-    est_a = librosa.lpc(filtered, 5)
-    assert np.allclose(truth_a, est_a, atol=1e-1, rtol=0)
+    for i in range(n):
+        noise = np.random.randn(1000)
+        filtered = scipy.signal.lfilter([1], truth_a, noise)
+        est_a[i, :] = librosa.lpc(filtered, 5)
+    assert np.allclose(truth_a, np.mean(est_a, axis=0), rtol=0, atol=1e-3)
 
 
 def test_to_mono():