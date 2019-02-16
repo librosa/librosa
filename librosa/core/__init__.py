fb9b435f5e4307622e4e9f60a6acbe57cd66aad2
Brian McFee <brian.mcfee@nyu.edu>
2019-02-16 10:23:41 -0500

    Added fft library configurator

--- librosa/core/__init__.py
+++ librosa/core/__init__.py
@@ -40,6 +40,9 @@ Spectral representations
     phase_vocoder
     magphase
 
+    get_fftlib
+    set_fftlib
+
 Magnitude scaling
 -----------------
 .. autosummary::
@@ -112,6 +115,7 @@ from .spectrum import *  # pylint: disable=wildcard-import
 from .pitch import *  # pylint: disable=wildcard-import
 from .constantq import *  # pylint: disable=wildcard-import
 from .harmonic import *  # pylint: disable=wildcard-import
+from .fft import *  # pylint: disable=wildcard-import
 
 from ..util.decorators import moved as _moved
 from ..util import fill_off_diagonal as _fod
