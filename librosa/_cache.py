380365ae00fe1c4c18cf559f085c10cdf007860b
Brian McFee <brian.mcfee@nyu.edu>
2019-02-20 15:41:56 -0500

    step 2 of cache refactor complete.  fixes #795

--- librosa/_cache.py
+++ librosa/_cache.py
@@ -6,16 +6,21 @@ import os
 from joblib import Memory
 
 
-class CacheManager(Memory):
-    '''The librosa cache manager class extends joblib.Memory
+class CacheManager(object):
+    '''The librosa cache manager class wraps joblib.Memory
     with a __call__ attribute, so that it may act as a function.
 
-    This allows us to override the librosa.cache module's __call__
-    field, thereby allowing librosa.cache to act as a decorator function.
+    Additionally, it provides a caching level filter, so that
+    different functions can be cached or not depending on the user's
+    preference for speed vs. storage usage.
     '''
 
-    def __init__(self, location, level=10, **kwargs):
-        super(CacheManager, self).__init__(location, **kwargs)
+    def __init__(self, *args, **kwargs):
+
+        level = kwargs.pop('level', 10)
+
+        # Initialize the memory object
+        self.memory = Memory(*args, **kwargs)
         # The level parameter controls which data we cache
         # smaller numbers mean less caching
         self.level = level
@@ -45,12 +50,27 @@ class CacheManager(Memory):
                     func, 'return decorated(%(signature)s)',
                     dict(decorated=dec(func)), __wrapped__=func)
 
-            if self.location is not None and self.level >= level:
-                return decorator_apply(self.cache, function)
+            if self.memory.location is not None and self.level >= level:
+                return decorator_apply(self.memory.cache, function)
 
             else:
                 return function
         return wrapper
+
+    def clear(self, *args, **kwargs):
+        return self.memory.clear(*args, **kwargs)
+
+    def eval(self, *args, **kwargs):
+        return self.memory.eval(*args, **kwargs)
+
+    def format(self, *args, **kwargs):
+        return self.memory.format(*args, **kwargs)
+
+    def reduce_size(self, *args, **kwargs):
+        return self.memory.reduce_size(*args, **kwargs)
+
+    def warn(self, *args, **kwargs):
+        return self.memory.warn(*args, **kwargs)
 
 
 # Instantiate the cache from the environment
