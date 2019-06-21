2327e7b406bcc4ada41f40dba832f8b9ef04fd2a
Brian McFee <brian.mcfee@nyu.edu>
2019-06-21 12:27:36 -0400

    partial fix of #804 expanding mfcc docs

--- librosa/feature/spectral.py
+++ librosa/feature/spectral.py
@@ -1616,6 +1616,23 @@ def mfcc(y=None, sr=22050, S=None, n_mfcc=20, dct_type=2, norm='ortho', **kwargs
            [  1.066e-14,  -7.500e+00, ...,   1.421e-14,   1.421e-14],
            [  3.109e-14,  -5.058e+00, ...,   2.931e-14,   2.931e-14]])
 
+    Using a different hop length and HTK-style Mel frequencies
+
+    >>> librosa.feature.mfcc(y=y, sr=sr, hop_length=1024, htk=True)
+    array([[-1.628e+02, -8.903e+01, -1.409e+02, ..., -1.078e+02,
+        -2.504e+02, -2.393e+02],
+       [ 1.275e+02,  9.532e+01,  1.019e+02, ...,  1.152e+02,
+         2.224e+02,  1.750e+02],
+       [ 1.139e+01,  6.155e+00,  1.266e+01, ...,  4.557e+01,
+         4.585e+01,  3.985e+01],
+       ...,
+       [ 3.462e+00,  4.032e+00, -5.694e-01, ..., -6.677e+00,
+        -1.183e-01,  1.485e+00],
+       [ 9.569e-01,  1.069e+00, -6.865e+00, ..., -9.598e+00,
+        -1.611e+00, -6.716e+00],
+       [ 8.457e+00,  3.582e+00, -1.156e-01, ..., -3.018e+00,
+        -1.456e+01, -6.991e+00]], dtype=float32)
+
     Use a pre-computed log-power Mel spectrogram
 
     >>> S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,
