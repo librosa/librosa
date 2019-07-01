9c628a19e96f96f7ce80a1222348848661cc9a4b
Brian McFee <brian.mcfee@nyu.edu>
2019-07-01 18:05:49 -0400

    working on doc build [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -410,9 +410,9 @@ autodoc_member_order = 'bysource'
 # Sphinx-contrib versioning
 # sphinx-versioning -l conf.py push docs gh-pages .
 import re
-#scv_whitelist_branches = ('master',)
-#scv_whitelist_tags = (re.compile(r'^v?\d\.\d\.\d+(rc\d+)?$'),)  # use this for RC builds
-scv_whitelist_tags = (re.compile(r'^v?\d\.\d\.\d+$'),)  # use this for final builds
+scv_whitelist_branches = ('master',)
+#scv_whitelist_tags = (re.compile(r'^v?\d\.\d\.\d+(\.?rc\d+)?$'),)  # use this for RC builds
+scv_whitelist_tags = (re.compile(r'^v?\d\.\d\.\d+$'), re.compile(r'^0\.7\.0.*?'))  # use this for final builds
 scv_greatest_tag = True
 scv_banner_greatest_tag = True
 scv_show_banner = True
