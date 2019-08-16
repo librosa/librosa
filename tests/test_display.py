0bc1ab52c0b5860dbd6d4ffc904a7c87dca69475
Brian McFee <brian.mcfee@nyu.edu>
2019-08-16 16:12:07 -0400

    fixed a frame-induced bug in waveplot and regenerated test images

--- tests/test_display.py
+++ tests/test_display.py
@@ -440,7 +440,7 @@ def test_waveplot_ext_axes(y):
 @pytest.mark.mpl_image_compare(baseline_images=['waveplot_stereo'], extensions=['png'])
 def test_waveplot_stereo(y, sr):
 
-    ys = np.vstack([y[np.newaxis, :], 2 * y[np.newaxis, :]])
+    ys = librosa.util.stack(y, 2 * y)
 
     plt.figure()
     librosa.display.waveplot(ys, sr=sr)
