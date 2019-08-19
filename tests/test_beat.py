a28533a28eafebc0f5a3a8299c13cb93f71dbdf8
Brian McFee <brian.mcfee@nyu.edu>
2019-08-19 16:05:03 -0400

    added unit tests for tempo and plp with priors

--- tests/test_beat.py
+++ tests/test_beat.py
@@ -14,6 +14,7 @@ except:
 import pytest
 
 import numpy as np
+import scipy.stats
 import librosa
 
 from test_core import files, load
@@ -21,54 +22,47 @@ from test_core import files, load
 __EXAMPLE_FILE = os.path.join('tests', 'data', 'test1_22050.wav')
 
 
-def test_onset_strength():
+@pytest.mark.parametrize('infile', files(os.path.join('data', 'beat-onset-*.mat')))
+def test_onset_strength(infile):
 
-    def __test(infile):
-        DATA = load(infile)
+    DATA = load(infile)
 
-        # Compute onset envelope using the same spectrogram
-        onsets = librosa.onset.onset_strength(y=None,
-                                              sr=8000,
-                                              S=DATA['D'],
-                                              lag=1,
-                                              max_size=1,
-                                              center=False,
-                                              detrend=True,
-                                              aggregate=np.mean)
+    # Compute onset envelope using the same spectrogram
+    onsets = librosa.onset.onset_strength(y=None,
+                                          sr=8000,
+                                          S=DATA['D'],
+                                          lag=1,
+                                          max_size=1,
+                                          center=False,
+                                          detrend=True,
+                                          aggregate=np.mean)
 
-        assert np.allclose(onsets[1:], DATA['onsetenv'][0])
-
-        pass
-
-    for infile in files(os.path.join('data','beat-onset-*.mat')):
-        yield (__test, infile)
+    assert np.allclose(onsets[1:], DATA['onsetenv'][0])
 
 
-def test_tempo():
+@pytest.mark.parametrize('tempo', [60, 80, 110, 160])
+@pytest.mark.parametrize('sr', [22050, 44100])
+@pytest.mark.parametrize('hop_length', [512, 1024])
+@pytest.mark.parametrize('ac_size', [4, 8])
+@pytest.mark.parametrize('aggregate', [None, np.mean])
+@pytest.mark.parametrize('prior', [None, scipy.stats.uniform(60, 240)])
+def test_tempo(tempo, sr, hop_length, ac_size, aggregate, prior):
 
-    def __test(tempo, sr, hop_length, ac_size, aggregate, y):
+    y = np.zeros(20 * sr)
+    delay = np.asscalar(librosa.time_to_samples(60./tempo, sr=sr))
+    y[::delay] = 1
 
-        tempo_est = librosa.beat.tempo(y=y, sr=sr, hop_length=hop_length,
-                                       ac_size=ac_size,
-                                       aggregate=aggregate)
+    tempo_est = librosa.beat.tempo(y=y, sr=sr, hop_length=hop_length,
+                                   ac_size=ac_size,
+                                   aggregate=aggregate,
+                                   prior=prior)
 
-        # Being within 5% for the stable frames is close enough
-        if aggregate is None:
-            win_size = int(ac_size * sr // hop_length)
-            assert np.all(np.abs(tempo_est[win_size:-win_size] - tempo) <= 0.05 * tempo)
-        else:
-            assert np.abs(tempo_est - tempo) <= 0.05 * tempo, (tempo, tempo_est)
-
-    for sr in [22050, 44100]:
-        for tempo in [40, 60, 80, 110, 160]:
-            # Make a pulse train at the target tempo
-            y = np.zeros(20 * sr)
-            delay = np.asscalar(librosa.time_to_samples(60./tempo, sr=sr))
-            y[::delay] = 1
-            for hop_length in [512, 1024]:
-                for ac_size in [4, 8]:
-                    for aggregate in [None, np.mean]:
-                        yield __test, tempo, sr, hop_length, ac_size, aggregate, y
+    # Being within 5% for the stable frames is close enough
+    if aggregate is None:
+        win_size = int(ac_size * sr // hop_length)
+        assert np.all(np.abs(tempo_est[win_size:-win_size] - tempo) <= 0.05 * tempo)
+    else:
+        assert np.abs(tempo_est - tempo) <= 0.05 * tempo, (tempo, tempo_est)
 
 
 @pytest.mark.xfail(raises=librosa.ParameterError)
@@ -93,24 +87,19 @@ def test_beat_no_onsets():
     assert len(beats) == 0
 
 
-def test_tempo_no_onsets():
+@pytest.mark.parametrize('start_bpm', [40, 60, 117, 235])
+@pytest.mark.parametrize('aggregate', [None, np.mean])
+@pytest.mark.parametrize('onsets', [np.zeros(30 * 22050 // 512)])
+@pytest.mark.parametrize('sr', [22050])
+@pytest.mark.parametrize('hop_length', [512])
+def test_tempo_no_onsets(start_bpm, aggregate, onsets, sr, hop_length):
 
-    sr = 22050
-    hop_length = 512
-    duration = 30
-    onsets = np.zeros(duration * sr // hop_length)
-
-    def __test(start_bpm, aggregate):
-        tempo = librosa.beat.tempo(onset_envelope=onsets, sr=sr,
-                                   hop_length=hop_length,
-                                   start_bpm=start_bpm,
-                                   aggregate=aggregate)
-        # Depending on bin resolution, we might not be able to match exactly
-        assert np.allclose(tempo, start_bpm, atol=1e0)
-
-    for start_bpm in [40, 60, 117, 235]:
-        for aggregate in [None, np.mean]:
-            yield __test, start_bpm, aggregate
+    tempo = librosa.beat.tempo(onset_envelope=onsets, sr=sr,
+                               hop_length=hop_length,
+                               start_bpm=start_bpm,
+                               aggregate=aggregate)
+    # Depending on bin resolution, we might not be able to match exactly
+    assert np.allclose(tempo, start_bpm, atol=1e0)
 
 
 def test_beat():
@@ -199,7 +188,8 @@ def test_beat_units(sr, hop_length, units):
                                                   (60, None),
                                                   pytest.mark.xfail((120, 80),
                                                       raises=librosa.ParameterError)])
-def test_plp(sr, hop_length, win_length, tempo_min, tempo_max, use_onset):
+@pytest.mark.parametrize('prior', [None, scipy.stats.lognorm(s=1, loc=np.log(120), scale=120)])
+def test_plp(sr, hop_length, win_length, tempo_min, tempo_max, use_onset, prior):
 
     y, sr = librosa.load(__EXAMPLE_FILE, sr=sr)
 
@@ -210,13 +200,15 @@ def test_plp(sr, hop_length, win_length, tempo_min, tempo_max, use_onset):
                                  hop_length=hop_length,
                                  win_length=win_length,
                                  tempo_min=tempo_min,
-                                 tempo_max=tempo_max)
+                                 tempo_max=tempo_max,
+                                 prior=prior)
     else:
         pulse = librosa.beat.plp(y=y, sr=sr,
                                  hop_length=hop_length,
                                  win_length=win_length,
                                  tempo_min=tempo_min,
-                                 tempo_max=tempo_max)
+                                 tempo_max=tempo_max,
+                                 prior=prior)
 
     assert len(pulse) == len(oenv)
 
@@ -227,19 +219,15 @@ def test_plp(sr, hop_length, win_length, tempo_min, tempo_max, use_onset):
 # Beat tracking regression test is no longer enabled due to librosa's
 # corrections
 @pytest.mark.skip
-def deprecated_test_beat():
+@pytest.mark.parametrize('infile', files(os.path.join('data', 'beat-beat-*.mat')))
+def deprecated_test_beat(infile):
 
-    def __test(infile):
+    DATA = load(infile)
 
-        DATA = load(infile)
+    (bpm, beats) = librosa.beat.beat_track(y=None,
+                                           sr=8000,
+                                           hop_length=32,
+                                           onset_envelope=DATA['onsetenv'][0])
 
-        (bpm, beats) = librosa.beat.beat_track(y=None,
-                                               sr=8000,
-                                               hop_length=32,
-                                               onset_envelope=DATA['onsetenv'][0])
-
-        beat_times = librosa.frames_to_time(beats, sr=8000, hop_length=32)
-        assert np.allclose(beat_times, DATA['beats'])
-
-    for infile in files(os.path.join('data','beat-beat-*.mat')):
-        yield (__test, infile)
+    beat_times = librosa.frames_to_time(beats, sr=8000, hop_length=32)
+    assert np.allclose(beat_times, DATA['beats'])
