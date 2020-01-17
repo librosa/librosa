a45b7c5e061b1a9696851d8840e7a3138d358b42
Brian McFee <brian.mcfee@nyu.edu>
2020-01-16 22:01:49 -0500

    removed python 2.7 CI

--- .travis_dependencies.sh
+++ .travis_dependencies.sh
@@ -23,11 +23,7 @@ if [ ! -d "$src" ]; then
     pushd $HOME/env
 
         # Download miniconda packages
-        if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-            wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-        else
-            wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-        fi
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 
         # Install both environments
         bash miniconda.sh -b -p $src
@@ -38,10 +34,6 @@ if [ ! -d "$src" ]; then
         source activate $ENV_NAME
 
         conda install -c conda-forge ffmpeg pysoundfile python-coveralls
-
-        if [[ "$TRAVIS_PYTHON_VERSION" == "3.5" ]]; then
-            conda install -c numba "numba>=0.43"
-        fi
 
         source deactivate
     popd
