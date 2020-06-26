59df759a6409d608ea625d226c086d8493d43e5a
Brian McFee <brian.mcfee@nyu.edu>
2020-06-26 18:02:45 -0400

    error correction in docstring examples

--- librosa/util/utils.py
+++ librosa/util/utils.py
@@ -1021,7 +1021,7 @@ def peak_pick(x, pre_max, post_max, pre_avg, post_avg, delta, wait):
     >>> import matplotlib.pyplot as plt
     >>> times = librosa.times_like(onset_env, sr=sr, hop_length=512)
     >>> fig, ax = plt.subplots(nrows=2, sharex=True)
-    >>> D = librosa.stft(y)
+    >>> D = np.abs(librosa.stft(y))
     >>> librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
     ...                          y_axis='log', x_axis='time', ax=ax[1])
     >>> ax[0].plot(times, onset_env, alpha=0.8, label='Onset strength')
