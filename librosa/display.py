f7b492d3ec938ee2063789da877942bfef01777f
Paul Biberstein <biberstein.paul@gmail.com>
2020-07-26 11:28:22 -0400

    Add support for 'fft_note' axis type

--- librosa/display.py
+++ librosa/display.py
@@ -39,7 +39,7 @@ import warnings
 import numpy as np
 from matplotlib.cm import get_cmap
 from matplotlib.axes import Axes
-from matplotlib.ticker import Formatter, ScalarFormatter
+from matplotlib.ticker import AutoLocator, Formatter, MultipleLocator, ScalarFormatter
 from matplotlib.ticker import LogLocator, FixedLocator, MaxNLocator
 from matplotlib.ticker import SymmetricalLogLocator
 
@@ -888,6 +888,7 @@ def __mesh_coords(ax_type, coords, n, **kwargs):
     coord_map = {
         "linear": __coord_fft_hz,
         "fft": __coord_fft_hz,
+        "fft_note": __coord_fft_hz,
         "hz": __coord_fft_hz,
         "log": __coord_fft_hz,
         "mel": __coord_mel_hz,
@@ -963,6 +964,12 @@ def __scale_axes(axes, ax_type, which):
     elif ax_type in ["cqt", "cqt_hz", "cqt_note", "cqt_svara"]:
         mode = "log"
         kwargs[base] = 2
+
+    elif ax_type == "fft_note":
+        mode = "symlog"
+        kwargs[base] = 2
+        kwargs[thresh] = core.note_to_hz("C2")
+        kwargs[scale] = 0.5
 
     elif ax_type in ["tempo", "fourier_tempo"]:
         mode = "log"
@@ -1088,6 +1095,18 @@ def __decorate_axis(axis, ax_type, key="C:maj", Sa=None, mela=None, thaat=None):
             LogLocator(base=2.0, subs=2.0 ** (np.arange(1, 12) / 12.0))
         )
         axis.set_label_text("Hz")
+
+    elif ax_type == "fft_note":
+        axis.set_major_formatter(NoteFormatter(key=key))
+        # Where is C1 relative to 2**k hz?
+        log_C1 = np.log2(core.note_to_hz("C1"))
+        C_offset = 2.0 ** (log_C1 - np.floor(log_C1))
+        axis.set_major_locator(SymmetricalLogLocator(axis.get_transform()))
+        axis.set_minor_formatter(NoteFormatter(key=key, major=False))
+        axis.set_minor_locator(
+            LogLocator(base=2.0, subs=2.0 ** (np.arange(1, 12) / 12.0))
+        )
+        axis.set_label_text("Note")
 
     elif ax_type in ["mel", "log"]:
         axis.set_major_formatter(ScalarFormatter())
