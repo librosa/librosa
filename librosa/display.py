ac9e4a494e076dbc3c4561cfb7e37190c488accc
N. Dorukhan Sergin <dorukhan.sergin@yandex.com>
2021-04-30 07:33:51 -0700

    Scale covariance, self-similarity etc. plots to square (#1312)
    * specshow squares certain plots
    
    * auto_scale to auto_aspect and updated docstring

--- librosa/display.py
+++ librosa/display.py
@@ -647,6 +647,7 @@ def specshow(
     Sa=None,
     mela=None,
     thaat=None,
+    auto_aspect=True,
     ax=None,
     **kwargs,
 ):
@@ -767,6 +768,12 @@ def specshow(
     thaat : str, optional
         If using `chroma_h` display mode, specify the parent thaat.
 
+    auto_aspect : bool
+        Axes will have 'equal' aspect if the horizontal and vertical dimensions 
+        cover the same extent and their types match.
+
+        To override, set to `False`.
+
     ax : matplotlib.axes.Axes or None
         Axes to plot on instead of the default `plt.gca()`.
 
@@ -859,6 +866,9 @@ def specshow(
     __decorate_axis(axes.xaxis, x_axis, key=key, Sa=Sa, mela=mela, thaat=thaat)
     __decorate_axis(axes.yaxis, y_axis, key=key, Sa=Sa, mela=mela, thaat=thaat)
 
+    # If the plot is a self-similarity/covariance etc. plot, square it
+    if __same_axes(x_axis, y_axis, axes.get_xlim(), axes.get_ylim()) and auto_aspect:
+        axes.set_aspect('equal')
     return out
 
 
@@ -1194,3 +1204,9 @@ def __coord_n(n, **_kwargs):
 def __coord_time(n, sr=22050, hop_length=512, **_kwargs):
     """Get time coordinates from frames"""
     return core.frames_to_time(np.arange(n + 1), sr=sr, hop_length=hop_length)
+
+def __same_axes(x_axis, y_axis, xlim, ylim):
+    """Check if two axes are the same, used to determine squared plots"""
+    axes_same_and_not_none = (x_axis == y_axis) and (x_axis is not None)
+    axes_same_lim = xlim == ylim
+    return axes_same_and_not_none and axes_same_lim
