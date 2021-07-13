6c1f30cdcbced92093047961d1fe47d37222eb56
Brian McFee <brian.mcfee@nyu.edu>
2021-07-13 11:25:53 -0400

    multichannel phase vocoding

--- tests/test_multichannel.py
+++ tests/test_multichannel.py
@@ -199,4 +199,17 @@ def test_spectral_centroid_multi_variable(s_multi):
     # Verify that they're not all the same
     assert not np.allclose(Call[0], Call[1])
 
+@pytest.mark.parametrize('rate', [0.5, 2])
+def test_phase_vocoder(y_multi, rate):
+    y, sr = y_multi
+    D = librosa.stft(y)
+
+    D0 = librosa.phase_vocoder(D[0], rate)
+    D1 = librosa.phase_vocoder(D[1], rate)
+    D2 = librosa.phase_vocoder(D, rate)
+
+    assert np.allclose(D2[0], D0)
+    assert np.allclose(D2[1], D1)
+    assert not np.allclose(D2[0], D2[1])
+
 
