fixes precision issues by looping over channels

librosa/util/_nnls.py
@@ -12,6 +12,7 @@
 import numpy as np
 import scipy.optimize
 from .utils import MAX_MEM_BLOCK
+from .exceptions import ParameterError
 
 
 __all__ = ["nnls"]
@@ -28,7 +29,7 @@ def _nnls_obj(x, shape, A, B):
     diff = np.einsum('mf,...ft->...mt',A,x, optimize=True) - B
 
     # Compute the objective value
-    value = 0.5 * np.sum(diff ** 2, axis=(-1,-2))
+    value = 0.5 * np.sum(diff ** 2)
 
     # And the gradient
     grad = np.einsum('fm,...mt->...ft',A.T,diff, optimize=True)
@@ -79,6 +80,23 @@ def _nnls_lbfgs_block(A, B, x_init=None, **kwargs):
     )
     # reshape the solution
     return x.reshape(shape)
+
+
+def _process_in_blocks(A, B, n_columns, **kwargs):
+    # Process in blocks:
+    if B.shape[-1] <= n_columns:
+        return _nnls_lbfgs_block(A, B, **kwargs).astype(A.dtype)
+    
+    x = np.einsum('fm,...mt->...ft', np.linalg.pinv(A), B, optimize=True)
+    np.clip(x, 0, None, out=x)
+    x_init = x
+    
+    for bl_s in range(0, x.shape[-1], n_columns):
+        bl_t = min(bl_s + n_columns, B.shape[-1])
+        x[..., bl_s:bl_t] = _nnls_lbfgs_block(
+            A, B[..., bl_s:bl_t], x_init=x_init[..., bl_s:bl_t], **kwargs
+        )
+    return x
 
 
 def nnls(A, B, **kwargs):
@@ -146,17 +164,13 @@ def nnls(A, B, **kwargs):
     n_columns = MAX_MEM_BLOCK // (A.shape[-1] * A.itemsize)
     n_columns = max(n_columns, 1)
 
-    # Process in blocks:
-    if B.shape[-1] <= n_columns:
-        return _nnls_lbfgs_block(A, B, **kwargs).astype(A.dtype)
-
-    x = np.einsum('fm,...mt->...ft', np.linalg.pinv(A), B, optimize=True)
-    np.clip(x, 0, None, out=x)
-    x_init = x
-
-    for bl_s in range(0, x.shape[-1], n_columns):
-        bl_t = min(bl_s + n_columns, B.shape[-1])
-        x[..., bl_s:bl_t] = _nnls_lbfgs_block(
-            A, B[..., bl_s:bl_t], x_init=x_init[..., bl_s:bl_t], **kwargs
-        )
-    return x
+    # if single channel
+    if B.ndim == 2:
+        x = _process_in_blocks(A,B,n_columns,**kwargs)    
+    elif B.ndim == 3:
+        x = []
+        for iB in B:
+            x.append(_process_in_blocks(A,iB,n_columns,**kwargs))
+    else:
+        raise ParameterError("Target matrix B should have ndim <= 3 but has B.ndim: {:d}".format(B.ndim))
+    return np.array(x)

tests/test_multichannel.py
@@ -20,7 +20,6 @@ import scipy.io
 import pytest
 import warnings
 from unittest import mock
-from test_core import srand
 
 from contextlib2 import nullcontext as dnr
 
@@ -912,21 +911,34 @@ def test_nnls_multi(s_multi):
     S, sr = s_multi
 
     # multichannel
-    M = librosa.feature.melspectrogram(S=S, sr=sr, power=1)
-    mel_basis = librosa.filters.mel(sr, n_fft=S.shape[-2], n_mels = M.shape[-2])
+    mel_basis = librosa.filters.mel(sr, n_fft=2*S.shape[-2]-1, n_mels = 32)
+    M = np.einsum('...ft,mf->...mt', S, mel_basis)
     S_recover = librosa.util.nnls(mel_basis, M)
 
     # channel 0
-    M0 = librosa.feature.melspectrogram(S=S[0], sr=sr, power=1)
+    M0 = np.einsum('...ft,mf->...mt', S[0], mel_basis)
     S0_recover = librosa.util.nnls(mel_basis, M0)
 
-    # chanel 1
-    M1 = librosa.feature.melspectrogram(S=S[1], sr=sr, power=1)
+    # channel 1
+    M1 = np.einsum('...ft,mf->...mt', S[1], mel_basis)
     S1_recover = librosa.util.nnls(mel_basis, M1)
 
     # Check each channel
-    assert np.allclose(S_recover[0], S0_recover, atol = 0.5, rtol = 0.5)
-    assert np.allclose(S_recover[1], S1_recover, atol = 0.5, rtol = 0.5)
+    assert np.allclose(S_recover[0], S0_recover)
+    assert np.allclose(S_recover[1], S1_recover)
 
     # Check that they're not both the same
-    assert not np.allclose(S0_recover, S1_recover, atol = 0.5, rtol = 0.5)
+    assert not np.allclose(S0_recover, S1_recover)
+
+
+@pytest.mark.xfail(raises=librosa.ParameterError)
+def test_nnls_badBndim(s_multi):
+
+    # Verify that a target matrix with ndim > 3 raises a ParameterError
+    S, sr = s_multi
+
+    # multichannel
+    mel_basis = librosa.filters.mel(sr, n_fft=2*S.shape[-2]-1, n_mels = 32)
+    M = np.einsum('...ft,mf->...mt', S, mel_basis)
+    S_recover = librosa.util.nnls(mel_basis, M[np.newaxis])
+