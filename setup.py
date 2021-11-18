492222c8bf9c58594f202993a5deda6034345617
Alex Malins <github@alexmalins.com>
2021-11-19 01:47:03 +0900

    move responsibility for fetching version num to setup.cfg (#1406)

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from importlib.machinery import SourceFileLoader
 from setuptools import setup
 
-
-version = SourceFileLoader('librosa.version',
-                           'librosa/version.py').load_module()
-
 if __name__ == '__main__':
-    setup(version=version.version)
+    setup()
