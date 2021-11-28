Fixing inverse cqt filterbank normalization

librosa/core/constantq.py
@@ -627,20 +627,24 @@ def icqt(
     # Get the top octave of frequencies
     n_bins = C.shape[-2]
 
+    n_octaves = int(np.ceil(float(n_bins) / bins_per_octave))
+
     # truncate the cqt to max frames if helpful
     freqs = cqt_frequencies(
         fmin=fmin, n_bins=n_bins, bins_per_octave=bins_per_octave
     )
     alpha = __bpo_to_alpha(bins_per_octave)
-    lengths, _ = filters.wavelet_lengths(
+
+    lengths, f_cutoff = filters.wavelet_lengths(
         freqs, sr=sr, window=window, filter_scale=filter_scale, alpha=alpha
     )
+
+    # Trim the CQT to only what's necessary for reconstruction
     if length is not None:
         n_frames = int(np.ceil((length + max(lengths)) / hop_length))
         C = C[..., :n_frames]
 
-    # How many octaves do we have?
-    n_octaves = int(np.ceil(float(n_bins) / bins_per_octave))
+    C_scale = np.sqrt(lengths)
 
     # This shape array will be used for broadcasting the basis scale
     # we'll have to adapt this per octave within the loop
@@ -667,12 +671,10 @@ def icqt(
 
         # Slice out the current octave
         sl = slice(bins_per_octave * i, bins_per_octave * i + n_filters)
-        C_oct = C[..., sl, :]
-        freq_oct = freqs[sl]
 
-        fft_basis, n_fft, lengths = __vqt_filter_fft(
+        fft_basis, n_fft, _ = __vqt_filter_fft(
             my_sr,
-            freq_oct,
+            freqs[sl],
             filter_scale,
             norm,
             sparsity,
@@ -681,24 +683,26 @@ def icqt(
             alpha=alpha,
         )
 
-        # Re-scale the filters to compensate for downsampling
-        fft_basis /= np.sqrt(2 ** (n_octaves - i))
-
+        # Transpose the basis
         inv_basis = fft_basis.H.todense()
 
-        if scale:
-            # This scaling is wrong when gamma != 0
-            C_scale = np.sqrt(lengths) * (n_fft / lengths) ** 2
-        else:
-            C_scale = (n_fft / lengths) ** 2
+       # Compute each filter's frequency-domain power
+        freq_power = 1/np.sum(np.abs(np.asarray(inv_basis))**2, axis=0)
+        
+        # Compensate for length normalization in the forward transform
+        freq_power *= n_fft / lengths[sl] 
 
         # Inverse-project the basis for each octave
-        D_oct = np.einsum("fc,c,...ct->...ft", inv_basis, C_scale, C_oct, optimize=True)
+        if scale:
+            # scale=True ==> re-scale by sqrt(lengths)
+            D_oct = np.einsum("fc,c,c,...ct->...ft", inv_basis, C_scale[sl], freq_power, C[..., sl, :], optimize=True)
+        else:
+            D_oct = np.einsum("fc,c,...ct->...ft", inv_basis, freq_power, C[..., sl, :], optimize=True)
 
         y_oct = istft(D_oct, window="ones", hop_length=my_hop, dtype=dtype)
 
         y_oct = audio.resample(
-            y_oct, 1, sr // my_sr, res_type=res_type, scale=not scale, fix=False
+            y_oct, 1, sr // my_sr, res_type=res_type, scale=False, fix=False
         )
 
         if y is None: