ecca6bee2b715fe63e6f00a7af07f1240393aa39
akiyuki ishikawa <aki.y.ishikawa@gmail.com>
2022-03-29 13:10:16 +0900

    Add minor fix to reduce ambiguity

--- librosa/core/spectrum.py
+++ librosa/core/spectrum.py
@@ -1503,7 +1503,7 @@ def power_to_db(S, *, ref=1.0, amin=1e-10, top_db=80.0):
 
     top_db : float >= 0 [scalar]
         threshold the output at ``top_db`` below the peak:
-        ``max(10 * log10(S)) - top_db``
+        ``max(10 * log10(S/ref)) - top_db``
 
     Returns
     -------
@@ -1633,7 +1633,7 @@ def db_to_power(S_db, *, ref=1.0):
 def amplitude_to_db(S, *, ref=1.0, amin=1e-5, top_db=80.0):
     """Convert an amplitude spectrogram to dB-scaled spectrogram.
 
-    This is equivalent to ``power_to_db(S**2, ref=ref**2, amin=amin**2)``, but is provided for convenience.
+    This is equivalent to ``power_to_db(S**2, ref=ref**2, amin=amin**2, top_db)``, but is provided for convenience.
 
     Parameters
     ----------
@@ -1652,7 +1652,7 @@ def amplitude_to_db(S, *, ref=1.0, amin=1e-5, top_db=80.0):
 
     top_db : float >= 0 [scalar]
         threshold the output at ``top_db`` below the peak:
-        ``max(20 * log10(S)) - top_db``
+        ``max(20 * log10(S/ref)) - top_db``
 
     Returns
     -------
