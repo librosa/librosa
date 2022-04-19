2bbdf41370908a645f88a18114d9e881d0713952
Brian McFee <brian.mcfee@nyu.edu>
2022-04-19 12:08:24 -0400

    fixed #1450, allow preconstructed audioread objects

--- librosa/core/audio.py
+++ librosa/core/audio.py
@@ -64,7 +64,7 @@ def load(
 
     Parameters
     ----------
-    path : string, int, pathlib.Path, soundfile.SoundFile or file-like object
+    path : string, int, pathlib.Path, soundfile.SoundFile, audioread object, or file-like object
         path to the input file.
 
         Any codec supported by `soundfile` or `audioread` will work.
@@ -75,8 +75,9 @@ def load(
         If the codec is supported by `soundfile`, then `path` can also be
         an open file descriptor (int) or an existing `soundfile.SoundFile` object.
 
-        On the contrary, if the codec is not supported by `soundfile`
-        (for example, MP3), then `path` must be a file path (string or `pathlib.Path`).
+        Pre-constructed audioread decoders are also supported here, see the example
+        below.  This can be used, for example, to force a specific decoder rather
+        than relying upon audioread to select one for you.
 
     sr : number > 0 [scalar]
         target sampling rate
@@ -143,6 +144,15 @@ def load(
     >>> sr
     22050
 
+    >>> # Load using an already open SoundFile object
+    >>> import soundfile
+    >>> sfo = soundfile.SoundFile(librosa.ex('brahms'))
+    >>> y, sr = librosa.load(sfo)
+
+    >>> # Load using an already open audioread object
+    >>> import audioread.ffdec  # Use ffmpeg decoder
+    >>> aro = audioread.ffdec.FFmpegAudioFile(librosa.ex('brahms'))
+    >>> y, sr = librosa.load(aro)
     """
 
     try:
@@ -167,9 +177,9 @@ def load(
             # Load the target number of frames, and transpose to match librosa form
             y = sf_desc.read(frames=frame_duration, dtype=dtype, always_2d=False).T
 
-    except RuntimeError as exc:
+    except (RuntimeError, TypeError) as exc:
         # If soundfile failed, try audioread instead
-        if isinstance(path, (str, pathlib.PurePath)):
+        if isinstance(path, tuple([str, pathlib.PurePath] + audioread.available_backends())):
             warnings.warn("PySoundFile failed. Trying audioread instead.", stacklevel=2)
             y, sr_native = __audioread_load(path, offset, duration, dtype)
         else:
@@ -195,7 +205,15 @@ def __audioread_load(path, offset, duration, dtype):
     """
 
     y = []
-    with audioread.audio_open(path) as input_file:
+
+    if isinstance(path, tuple(audioread.available_backends())):
+        # If we have an audioread object already, don't bother opening
+        reader = path
+    else:
+        # If the input was not an audioread object, try to open it
+        reader = audioread.audio_open(path)
+
+    with reader as input_file:
         sr_native = input_file.samplerate
         n_channels = input_file.channels
 
