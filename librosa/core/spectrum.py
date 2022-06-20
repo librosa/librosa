77b9b13c667dbadc8244db33e4f36a37bc24f776
Will Monroe <wmonroe4@cs.stanford.edu>
2022-06-20 16:52:39 -0400

    Use `dtype` parameter to `empty_like`

--- librosa/core/spectrum.py
+++ librosa/core/spectrum.py
@@ -1169,16 +1169,13 @@ def magphase(D, *, power=1):
 
     mag = np.abs(D)
 
-    if not np.iscomplexobj(D):
-        return mag, np.sign(D, dtype=util.dtype_r2c(D.dtype)) + (D == 0)
-
     # Prevent NaNs and return magnitude 0, phase 1+0j for zero
     zeros_to_ones = mag == 0
     mag_nonzero = mag + zeros_to_ones
     # Compute real and imaginary separately, because complex division can
     # produce NaNs when denormalized numbers are involved (< ~2e-39 for
     # complex64, ~5e-309 for complex128)
-    phase = np.empty_like(D)
+    phase = np.empty_like(D, dtype=util.dtype_r2c(D.dtype))
     phase.real = D.real / mag_nonzero + zeros_to_ones
     phase.imag = D.imag / mag_nonzero
 
