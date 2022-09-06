febe8059719aad6f661a21d2f32625b31499bc86
Brian McFee <brian.mcfee@nyu.edu>
2022-09-06 19:16:48 -0400

    switched minimal environment to use soxr from pypi

--- librosa/core/constantq.py
+++ librosa/core/constantq.py
@@ -950,7 +950,9 @@ def vqt(
 
     if res_type is None:
         warnings.warn("Support for VQT with res_type=None is deprecated in librosa 0.10\n"
-                      "and will be removed in version 1.0.")
+                      "and will be removed in version 1.0.",
+                      category=DeprecationWarning,
+                      stacklevel=2)
         res_type = "soxr_hq"
 
     y, sr, hop_length = __early_downsample(
