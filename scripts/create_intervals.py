76556d9d971b910a2e218bdbf8e42b87eec30445
Brian McFee <brian.mcfee@nyu.edu>
2022-10-13 14:39:13 -0400

    updated interval pack

--- scripts/create_intervals.py
+++ scripts/create_intervals.py
@@ -16,47 +16,64 @@ The output will be stored in intervals.pickle
 """
 
 import msgpack
-import sympy
 import numpy as np
 import librosa
-
-
-def fraction(x):
-    """Wrapper to find a rational approximation to floating point numbers"""
-
-    return sympy.nsimplify(np.array(x), rational=True, full=True, tolerance=1e-4)
 
 
 def main():
 
     # Get the intervals
     intervals_pythagorean = librosa.pythagorean_intervals(
-        bins_per_octave=72, sort=False
+        bins_per_octave=72, sort=False, return_factors=True
     )
     intervals_3lim = librosa.plimit_intervals(
-        primes=[3], bins_per_octave=72, sort=False
+        primes=[3],
+        bins_per_octave=72,
+        sort=False,
+        return_factors=True,
     )
     intervals_5lim = librosa.plimit_intervals(
-        primes=[3, 5], bins_per_octave=72, sort=False
+        primes=[3, 5],
+        bins_per_octave=72,
+        sort=False,
+        return_factors=True,
     )
     intervals_7lim = librosa.plimit_intervals(
-        primes=[3, 5, 7], bins_per_octave=72, sort=False
+        primes=[3, 5, 7],
+        bins_per_octave=72,
+        sort=False,
+        return_factors=True,
     )
     intervals_23lim = librosa.plimit_intervals(
-        primes=[3, 5, 7, 11, 13, 17, 19, 23], bins_per_octave=190, sort=False
+        primes=[3, 5, 7, 11, 13, 17, 19, 23],
+        bins_per_octave=190,
+        sort=False,
+        return_factors=True,
     )
 
     all_intervals = np.concatenate(
-        (intervals_pythagorean, intervals_3lim, intervals_5lim, intervals_7lim, intervals_23lim)
+        (
+            intervals_pythagorean,
+            intervals_3lim,
+            intervals_5lim,
+            intervals_7lim,
+            intervals_23lim,
+        )
     )
-    # Convert to rationals
-    fractions = fraction(all_intervals)
 
     # Factorize the rationals and cache them, keyed by truncated float
-    factorized = {
-        np.around(interval, 6): sympy.factorrat(fraction)
-        for (interval, fraction) in zip(all_intervals, fractions)
-    }
+
+    factorized = dict()
+    for interval in all_intervals:
+        # Compute the interval
+        log_value = 0
+        for p in interval:
+            log_value += np.log2(p) * interval[p]
+        value = np.around(np.power(2.0, log_value), 6)
+
+        factorized[float(value)] = {
+            int(p): int(interval[p]) for p in interval if interval[p] != 0
+        }
 
     with open("intervals.msgpack", "wb") as fdesc:
         msgpack.dump(factorized, fdesc)
