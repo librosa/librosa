5411a12a2ec1fdbfc8af0d71554a19fa97d4c3b1
wendywang3315 <yutong_wang3315@outlook.com>
2022-12-06 14:29:05 -0500

    modified rainbowgram to visualize the demodulated phase differential

--- docs/examples/plot_rainbowgram.py
+++ docs/examples/plot_rainbowgram.py
@@ -7,11 +7,9 @@ This notebook demonstrates how to use "Rainbowgrams" to simultaneously
 visualize amplitude and (unwrapped) phase (differential).
 Our working example will be the problem of silence/non-silence detection.
 """
-
+# %%
 # Code source: Brian McFee
 # License: ISC
-
-##################
 # Standard imports
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,11 +18,19 @@ import librosa
 import librosa.display
 
 #############################################
+# %% 
+# Construsct a sine-sweep signal.
 sr = 22050
 y = librosa.chirp(fmin=32, fmax=32 * 2**5, sr=sr, duration=10, linear=True)
 D = librosa.stft(y)
 mag, phase = librosa.magphase(D)
 
+freqs = librosa.fft_frequencies()
+times = librosa.times_like(D)
+
+phase_exp = 2*np.pi*np.multiply.outer(freqs,times)
+
+# %%
 plt.close('all')
 fig, ax = plt.subplots()
 
@@ -39,7 +45,8 @@ ax.set_facecolor('#000')
 cbar = fig.colorbar(img, ticks=[-np.pi, -np.pi/2, 0, np.pi/2, np.pi])
 cbar.ax.set(yticklabels=['-π', '-π/2', 0, 'π/2', 'π']);
 
-
+# %%
+fig, ax = plt.subplots()
 img = librosa.display.specshow(np.diff(np.unwrap(np.angle(phase), axis=1), axis=1, prepend=0),
                          cmap='twilight', 
                          alpha=librosa.amplitude_to_db(mag, ref=np.max)/80 + 1,
@@ -47,3 +54,5 @@ img = librosa.display.specshow(np.diff(np.unwrap(np.angle(phase), axis=1), axis=
                          y_axis='log', 
                          x_axis='time')
 ax.set_facecolor('#888')
+
+# %%
