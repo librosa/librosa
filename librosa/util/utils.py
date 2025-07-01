88286a63bb14efb38fa454a050e701a2c1c7b758
Brian McFee <brian.mcfee@nyu.edu>
2025-07-01 11:42:22 -0400

    arguing with stubs

--- librosa/util/utils.py
+++ librosa/util/utils.py
@@ -1555,8 +1555,7 @@ def sparsify_rows(
     if dtype is None:
         dtype = x.dtype
 
-    x_sparse: scipy.sparse.lil_matrix = scipy.sparse.lil_matrix(x.shape, dtype=dtype)  #
-    type: ignore
+    x_sparse: scipy.sparse.lil_matrix = scipy.sparse.lil_matrix(x.shape, dtype=dtype)  # type: ignore
 
     mags = np.abs(x)
     norms = np.sum(mags, axis=1, keepdims=True)
