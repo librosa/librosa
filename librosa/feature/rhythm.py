3de0470e6f1d9b86d6a604c0084920d0d3234ac2
danferns <dannywritescode@gmail.com>
2025-12-09 14:38:50 +0530

    set np.expand_dims to -3 instead of using a param

--- librosa/feature/rhythm.py
+++ librosa/feature/rhythm.py
@@ -666,7 +666,6 @@ def metrogram(
     aggregate: Optional[Callable[..., Any]] = np.sum,
     kind: str = "linear",
     fill_value: float = 0,
-    axis: int = -2
 ) -> np.ndarray:
     """Metrogram Transform. [1]_
 
@@ -713,8 +712,6 @@ def metrogram(
     fill_value : float
         The value to fill when extrapolating beyond the observed
         tempo range.
-    axis : int
-        The tempogram axis representing tempo or frequency.
 
     Returns
     -------
@@ -736,18 +733,13 @@ def metrogram(
             [1 / 3, 1 / 4, 1 / 5, 1 / 7]
         )
 
-    if axis < -tg.ndim or axis >= tg.ndim:
-        raise ParameterError(
-            f"axis={axis} is out of range for tempogram dimensions ndim={tg.ndim}"
-        )
-
     tg_interp = interp_harmonics(
-        tg, freqs=freqs, harmonics=factors, kind=kind, fill_value=fill_value, axis=axis
+        tg, freqs=freqs, harmonics=factors, kind=kind, fill_value=fill_value, axis=-2
     )
-    
-    product = tg_interp * np.expand_dims(tg, axis=axis)
+
+    product = tg_interp * np.expand_dims(tg, axis=-3)
 
     if aggregate is not None:
-        return aggregate(product, axis=axis)
+        return aggregate(product, axis=-2)
 
     return product
