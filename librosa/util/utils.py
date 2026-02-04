fix: input shape validation based on broadcasting rules
Co-authored-by: Brian McFee <bmcfee@users.noreply.github.com>

librosa/util/utils.py
@@ -2808,22 +2808,11 @@ def interp_broadcast(
     if interp_pos is None:
         interp_pos = x1_pos
 
-    if x1.ndim != x2.ndim:
+    min_ndim = min(x1.ndim, x2.ndim)
+    if axis < -min_ndim or axis >= min_ndim:
         raise ParameterError(
-            f"x1 (ndim={x1.ndim}) and x2 (ndim={x2.ndim}) have a different number of dimensions."
+            f"axis={axis} is out of range for minimum ndim={min_ndim}"
         )
-
-    if axis < -x1.ndim or axis >= x1.ndim:
-        raise ParameterError(
-            f"axis={axis} is out of range for array dimensions ndim={x1.ndim}"
-        )
-
-    for i in range(x1.ndim):
-        if x1.shape[i] != x2.shape[i] and i != (axis + x1.ndim) % x1.ndim:
-            raise ParameterError(
-                f"x1.shape={x1.shape} and x2.shape={x2.shape} would remain "
-                f"broadcast incompatible after interpolating along axis={axis}."
-            )
 
     x1_interp = scipy.interpolate.interp1d(
         x1_pos,
@@ -2850,5 +2839,14 @@ def interp_broadcast(
 
     if op is None:
         return y1, y2
+
+    try:
+        np.broadcast_shapes(y1.shape, y2.shape)
+    except ValueError as exc:
+        raise ParameterError(
+            f"Interpolating x1.shape={x1.shape} and x2.shape={x2.shape} along "
+            f"axis={axis} leads to y1.shape={y1.shape} and y2.shape={y2.shape}, "
+            "which are not broadcast compatible."
+        ) from exc
 
     return op(y1, y2)

tests/test_util.py
@@ -1561,23 +1561,39 @@ def test_interp_broadcast_op(interp_arrays):
     assert np.allclose(result, np.add(y1, y2))
 
 
-@pytest.mark.xfail(raises=librosa.ParameterError)
-def test_interp_broadcast_dims():
-    librosa.util.interp_broadcast(
+def test_interp_broadcast_missing_dims():
+    # test with missing dims
+    result = librosa.util.interp_broadcast(
         x1=np.zeros((9, 1)),
         x1_pos=np.arange(9),
-        x2=np.zeros((1, 8, 1)),
+        x2=np.zeros((5, 8, 1)),
         x2_pos=np.arange(8),
+        axis=-2
     )
+    assert result.shape == (5, 9, 1)
+
+
+def test_interp_broadcast_compatible_shape():
+    # test with unequal dims, if one dim is 1
+    result = librosa.util.interp_broadcast(
+        x1=np.zeros((2, 9, 10)),
+        x1_pos=np.arange(9),
+        x2=np.zeros((2, 8, 1)),
+        x2_pos=np.arange(8),
+        axis=-2
+    )
+    assert result.shape == (2, 9, 10)
 
 
 @pytest.mark.xfail(raises=librosa.ParameterError)
-def test_interp_broadcast_shape():
+def test_interp_broadcast_incompatible_shape():
+    # test with unequal dims, neither dim is 1
     librosa.util.interp_broadcast(
-        x1=np.zeros((2, 9, 1)),
+        x1=np.zeros((2, 9, 10)),
         x1_pos=np.arange(9),
-        x2=np.zeros((1, 8, 1)),
+        x2=np.zeros((2, 8, 11)),
         x2_pos=np.arange(8),
+        axis=-2
     )
 
 