74b90136afec87ae2a6c8eebacd52d1663d20377
Brian McFee <brian.mcfee@nyu.edu>
2026-05-19 07:33:17 -0400

    velinizing

--- librosa/display.py
+++ librosa/display.py
@@ -3308,7 +3308,7 @@ def multiplot(
         number of arrays will determine the layout of the multiplot grid.
 
     axes : matplotlib.axes.Axes, np.ndarray, or None
-        The axes to use for the multiplot. If None, a new axes array will be created on `fig`. 
+        The axes to use for the multiplot. If None, a new axes array will be created on `fig`.
         If an array of Axes objects is provided, it must be compatible with the shape of the data.
         If a single axes object is provided, it will be interpreted as a 1x1 array (i.e. a single subplot).
 
