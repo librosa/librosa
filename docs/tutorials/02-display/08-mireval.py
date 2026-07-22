bab22cf200f308b0838a7db0b467ae4108370685
Brian McFee <brian.mcfee@nyu.edu>
2026-07-22 15:10:41 -0400

    updated multichannel section

--- docs/tutorials/02-display/08-mireval.py
+++ docs/tutorials/02-display/08-mireval.py
@@ -3,9 +3,11 @@
 """
 Annotations and mir_eval integration
 ====================================
-
+This section illustrates integration of librosa display with the `mir_eval` library's utilities
+for visualizing annotations.
 """
 
 # %%
 # .. _tutorial-display-mireval:
 #
+
