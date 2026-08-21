57643ccff2b6b706e8595edf6a24a946a45a6af7
Brian McFee <brian.mcfee@nyu.edu>
2026-08-21 10:04:52 -0400

    making linkcheck failures not catastrophic for CI

--- docs/conf.py
+++ docs/conf.py
@@ -467,7 +467,7 @@ linkcheck_timeout = 10
 linkcheck_workers = 10
 linkcheck_report_timeouts_as_broken = False
 linkcheck_ignore = ["https://www.ee.columbia.edu/~dpwe/resources/.*",
-                    "https://htk.eng.cam.ac.uk/.*",
+                    #"https://htk.eng.cam.ac.uk/.*",
                     # Ignore broken links to publishers, as they throttle scrapers
                     "https://zenodo.org/.*",
                     "https://transactions.ismir.net/.*"]
